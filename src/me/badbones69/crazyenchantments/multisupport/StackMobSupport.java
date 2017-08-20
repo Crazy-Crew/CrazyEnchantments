56300f261ab98aed73a38de533c498587bfec6bb
BadBones69 <joewojcik14@gmail.com>
2017-08-19 23:45:31 -0400

    v1.7.9.3.1 Update
    Bug Fixes:
      - Faction plugins that are not supported by CE sometimes caused errors due to how CE finds the differne between Factions by MassiveCraft and FactionsUUID.
    
    Removed:
      - StackMob has lost support due to the Dev removing the API from the plugin. Will re-add support if the API is re-added.

--- src/me/badbones69/crazyenchantments/multisupport/StackMobSupport.java
+++ src/me/badbones69/crazyenchantments/multisupport/StackMobSupport.java
@@ -1,21 +1,16 @@
 package me.badbones69.crazyenchantments.multisupport;
 
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-
-import me.badbones69.crazyenchantments.enchantments.Armor;
-import uk.antiperson.stackmob.api.events.EntityStackEvent;
 
 public class StackMobSupport implements Listener{
 	
-	@EventHandler
-	public void onStack(EntityStackEvent e){
-		for(Player player : Armor.getAllies().keySet()){
-			if(Armor.getAllies().get(player).contains(e.getFirstEntity().getEntity()) || Armor.getAllies().get(player).contains(e.getOtherEntity().getEntity())){
-				e.setCancelled(true);
-			}
-		}
-	}
+//	@EventHandler
+//	public void onStack(EntityStackEvent e){
+//		for(Player player : Armor.getAllies().keySet()){
+//			if(Armor.getAllies().get(player).contains(e.getFirstEntity().getEntity()) || Armor.getAllies().get(player).contains(e.getOtherEntity().getEntity())){
+//				e.setCancelled(true);
+//			}
+//		}
+//	}
 	
 }
