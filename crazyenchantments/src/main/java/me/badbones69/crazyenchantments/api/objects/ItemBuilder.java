cdd0913710df119c490c8344e33e0b492ef08b07
BadBones69 <joewojcik14@gmail.com>
2018-01-18 21:03:16 -0500

    v1.7.9.3.5 Update
    Bug Fixes:
     - Older Spigot/PaperSpigot servers would get an error when loading the plugin.

--- crazyenchantments/src/main/java/me/badbones69/crazyenchantments/api/objects/ItemBuilder.java
+++ crazyenchantments/src/main/java/me/badbones69/crazyenchantments/api/objects/ItemBuilder.java
@@ -431,7 +431,7 @@ public class ItemBuilder {
 		itemMeta.setDisplayName(getUpdatedName());
 		itemMeta.setLore(getUpdatedLore());
 		itemMeta.addItemFlags(flags.toArray(new ItemFlag[flags.size()]));
-		itemMeta.setUnbreakable(unbreakable);
+		itemMeta.spigot().setUnbreakable(unbreakable);
 		item.setItemMeta(itemMeta);
 		item.addUnsafeEnchantments(enchantments);
 		addGlow(item, glowing);
