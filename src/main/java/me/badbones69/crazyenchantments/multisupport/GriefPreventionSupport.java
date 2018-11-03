23031674ca300f430dcbb487920aa3a4e268f26d
pmme <gpaulmcs@gmail.com>
2018-11-03 19:27:04 +1300

    Fix NullPointerException from GriefPreventionSupport and TownySupport

--- src/main/java/me/badbones69/crazyenchantments/multisupport/GriefPreventionSupport.java
+++ src/main/java/me/badbones69/crazyenchantments/multisupport/GriefPreventionSupport.java
@@ -1,5 +1,6 @@
 package me.badbones69.crazyenchantments.multisupport;
 
+import me.ryanhamshire.GriefPrevention.Claim;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -9,16 +10,18 @@ public class GriefPreventionSupport {
 	private static GriefPrevention gp = GriefPrevention.instance;
 	
 	public static Boolean inTerritory(Player player) {
-		return gp.dataStore.getClaimAt(player.getLocation(), true, null).getOwnerName().equalsIgnoreCase(player.getName()) ||
-		gp.dataStore.getClaimAt(player.getLocation(), true, null).allowAccess(player) == null;
+		Claim claim = gp.dataStore.getClaimAt( player.getLocation(), true, null );
+		return claim != null && ( claim.getOwnerName().equalsIgnoreCase( player.getName() ) || claim.allowAccess( player ) == null );
 	}
 	
 	public static boolean isFriendly(Player player, Player other) {
-		return gp.dataStore.getClaimAt(player.getLocation(), true, null).allowAccess(other) == null;
+		Claim claim = gp.dataStore.getClaimAt( player.getLocation(), true, null );
+		return claim != null && claim.allowAccess( other ) == null;
 	}
 	
 	public static boolean canBreakBlock(Player player, Block block) {
-		return gp.dataStore.getClaimAt(player.getLocation(), true, null).allowEdit(player) == null;
+		Claim claim = gp.dataStore.getClaimAt( player.getLocation(), true, null );
+		return claim == null || claim.allowEdit( player ) == null;
 	}
 	
 }
