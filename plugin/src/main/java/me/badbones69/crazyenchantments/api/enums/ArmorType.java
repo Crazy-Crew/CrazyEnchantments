b3f9fa3f87c444afaf7b745732fd6a4c67ee50eb
BadBones69 <joewojcik14@gmail.com>
2019-07-04 04:09:10 -0400

    Updating ArmorType class

--- plugin/src/main/java/me/badbones69/crazyenchantments/api/enums/ArmorType.java
+++ plugin/src/main/java/me/badbones69/crazyenchantments/api/enums/ArmorType.java
@@ -1,20 +1,17 @@
 package me.badbones69.crazyenchantments.api.enums;
 
-import me.badbones69.crazyenchantments.api.CrazyEnchantments;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
 /**
- * @Author Borlea
- * @Github https://github.com/borlea/
- * @Website http://thederpygolems.ca/
- * Jul 30, 2015 6:46:16 PM
+ * @author Arnah
+ * @since Jul 30, 2015
  */
 public enum ArmorType {
 	
 	HELMET(5), CHESTPLATE(6), LEGGINGS(7), BOOTS(8);
 	
 	private final int slot;
-	private static CrazyEnchantments ce = CrazyEnchantments.getInstance();
 	
 	private ArmorType(int slot) {
 		this.slot = slot;
@@ -27,22 +24,13 @@ public enum ArmorType {
 	 * @return The parsed ArmorType. (null if none were found.)
 	 */
 	public static ArmorType matchType(final ItemStack itemStack) {
-		if(itemStack == null) {
-			return null;
-		}
-		if(itemStack.getType().name().toLowerCase().contains("skull")) {
-			return HELMET;
-		}
-		if(EnchantmentType.HELMET.getItems().contains(itemStack.getType())) {
-			return HELMET;
-		}else if(EnchantmentType.CHESTPLATE.getItems().contains(itemStack.getType())) {
-			return CHESTPLATE;
-		}else if(EnchantmentType.LEGGINGS.getItems().contains(itemStack.getType())) {
-			return LEGGINGS;
-		}else if(EnchantmentType.BOOTS.getItems().contains(itemStack.getType())) {
-			return BOOTS;
-		}
-		return null;
+		if(itemStack == null || itemStack.getType().equals(Material.AIR)) return null;
+		String type = itemStack.getType().name();
+		if(type.endsWith("_HELMET") || type.endsWith("_SKULL")) return HELMET;
+		else if(type.endsWith("_CHESTPLATE")) return CHESTPLATE;
+		else if(type.endsWith("_LEGGINGS")) return LEGGINGS;
+		else if(type.endsWith("_BOOTS")) return BOOTS;
+		else return null;
 	}
 	
 	public int getSlot() {
