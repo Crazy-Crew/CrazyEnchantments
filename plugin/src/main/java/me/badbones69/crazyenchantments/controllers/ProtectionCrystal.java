87e0e5def69544b1f588cb55b9dcf68156dc725f
BadBones69 <joewojcik14@gmail.com>
2019-07-04 04:34:02 -0400

    Lowered priority of ProtectionCrystal event.

--- plugin/src/main/java/me/badbones69/crazyenchantments/controllers/ProtectionCrystal.java
+++ plugin/src/main/java/me/badbones69/crazyenchantments/controllers/ProtectionCrystal.java
@@ -66,7 +66,7 @@ public class ProtectionCrystal implements Listener {
 		}
 	}
 	
-	@EventHandler(priority = EventPriority.MONITOR)
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerDeath(PlayerDeathEvent e) {
 		Player player = e.getEntity();
 		ArrayList<ItemStack> items = new ArrayList<>();
