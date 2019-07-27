0f8372e688988ec697dd82dd5a84d33db247b443
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2019-07-26 20:08:54 -0400

    added legacy plotsupport

---
+++ v1_12_2_down/src/main/java/me/badbones69/crazyenchantments/multisupport/plotsquared/PlotSquaredSupportLegacy.java
@@ -0,0 +1,13 @@
+package me.badbones69.crazyenchantments.multisupport.plotsquared;
+
+import com.intellectualcrafters.plot.object.Plot;
+import com.intellectualcrafters.plot.object.PlotPlayer;
+import org.bukkit.entity.Player;
+
+public class PlotSquaredSupportLegacy {
+
+    public static boolean inTerritory(Player player) {
+        Plot plot = PlotPlayer.get(player.getName()).getCurrentPlot();
+        return plot.getOwners().contains(player.getUniqueId()) || plot.getMembers().contains(player.getUniqueId());
+    }
+}
