c9d00b03fe98e2c2e5b2065a4a68fd965ed7694f
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2019-07-26 20:02:24 -0400

    Hopefully fixed it

--- plugin/src/main/java/me/badbones69/crazyenchantments/multisupport/PlotSquaredSupport.java
+++ plugin/src/main/java/me/badbones69/crazyenchantments/multisupport/PlotSquaredSupport.java
@@ -1,7 +1,7 @@
 package me.badbones69.crazyenchantments.multisupport;
 
-import com.intellectualcrafters.plot.object.Plot;
-import com.intellectualcrafters.plot.object.PlotPlayer;
+import com.github.intellectualsites.plotsquared.plot.object.Plot;
+import com.github.intellectualsites.plotsquared.plot.object.PlotPlayer;
 import org.bukkit.entity.Player;
 
 public class PlotSquaredSupport {
@@ -10,5 +10,4 @@ public class PlotSquaredSupport {
 		Plot plot = PlotPlayer.get(player.getName()).getCurrentPlot();
 		return plot.getOwners().contains(player.getUniqueId()) || plot.getMembers().contains(player.getUniqueId());
 	}
-	
 }
