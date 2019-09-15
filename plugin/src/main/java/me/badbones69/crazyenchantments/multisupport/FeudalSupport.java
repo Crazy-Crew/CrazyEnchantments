a4a8ca020d8cfccdbdb756a605c585c11a2d648e
BadBones69 <joewojcik14@gmail.com>
2019-09-15 05:21:09 -0400

    Updated to new Feudal api.

--- plugin/src/main/java/me/badbones69/crazyenchantments/multisupport/FeudalSupport.java
+++ plugin/src/main/java/me/badbones69/crazyenchantments/multisupport/FeudalSupport.java
@@ -1,19 +1,19 @@
 package me.badbones69.crazyenchantments.multisupport;
 
+import de.browniecodez.feudal.main.Main;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import us.forseth11.feudal.core.Feudal;
 import us.forseth11.feudal.kingdoms.Kingdom;
 import us.forseth11.feudal.kingdoms.Land;
 
 public class FeudalSupport {
 	
 	public static boolean isFrendly(Player player, Player other) {
-		Kingdom p = Feudal.getPlayerKingdom(player.getUniqueId().toString());
-		if(Feudal.getPlayerKingdom(other.getUniqueId().toString()) == null) {
+		Kingdom p = Main.getPlayerKingdom(player.getUniqueId().toString());
+		if(Main.getPlayerKingdom(other.getUniqueId().toString()) == null) {
 			return false;
 		}
-		Kingdom o = Feudal.getPlayerKingdom(other.getUniqueId().toString());
+		Kingdom o = Main.getPlayerKingdom(other.getUniqueId().toString());
 		return o != null && isFrendly(p, o);
 	}
 	
@@ -22,14 +22,14 @@ public class FeudalSupport {
 	}
 	
 	public static boolean inTerritory(Player player) {
-		Kingdom p = Feudal.getPlayerKingdom(player.getUniqueId().toString());
+		Kingdom p = Main.getPlayerKingdom(player.getUniqueId().toString());
 		return p != null && p.isOnLand(player.getLocation());
 	}
 	
 	public static boolean canBreakBlock(Player player, Block block) {
-		Kingdom p = Feudal.getPlayerKingdom(player.getUniqueId().toString());
+		Kingdom p = Main.getPlayerKingdom(player.getUniqueId().toString());
 		Land land = new Land(block.getLocation());
-		Kingdom b = Feudal.getLandKingdom(land);
+		Kingdom b = Main.getLandKingdom(land);
 		return p == b || b == null;
 	}
 	
