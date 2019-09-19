584372b0d0bc3844d97cc5a045f723a5e21fc82d
BadBones69 <joewojcik14@gmail.com>
2019-09-19 03:04:02 -0400

    Optimizes AuraListener class to be more efficient and easier to read.

--- plugin/src/main/java/me/badbones69/crazyenchantments/controllers/AuraListener.java
+++ plugin/src/main/java/me/badbones69/crazyenchantments/controllers/AuraListener.java
@@ -3,6 +3,7 @@ package me.badbones69.crazyenchantments.controllers;
 import me.badbones69.crazyenchantments.api.CrazyEnchantments;
 import me.badbones69.crazyenchantments.api.enums.CEnchantments;
 import me.badbones69.crazyenchantments.api.events.AuraActiveEvent;
+import me.badbones69.crazyenchantments.api.objects.CEnchantment;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -13,26 +14,32 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class AuraListener implements Listener {
 	
 	private CrazyEnchantments ce = CrazyEnchantments.getInstance();
+	private static List<CEnchantments> auraEnchantments = Arrays.asList(
+	CEnchantments.BLIZZARD,
+	CEnchantments.ACIDRAIN,
+	CEnchantments.SANDSTORM,
+	CEnchantments.RADIANT,
+	CEnchantments.INTIMIDATE);
 	
-	@EventHandler(priority = EventPriority.MONITOR)
+	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerMoveEvent(PlayerMoveEvent e) {
 		if(!e.isCancelled()) {
 			if((e.getFrom().getBlockX() != e.getTo().getBlockX()) || (e.getFrom().getBlockY() != e.getTo().getBlockY()) || (e.getFrom().getBlockZ() != e.getTo().getBlockZ())) {
-				ArrayList<Player> players = getNearByPlayers(e.getPlayer(), 3);
-				for(ItemStack item : e.getPlayer().getEquipment().getArmorContents()) {
-					Player player = e.getPlayer();
-					if(ce.hasEnchantments(item)) {
-						for(CEnchantments enchant : getAuraEnchantments()) {
-							if(ce.hasEnchantment(item, ce.getEnchantmentFromName(enchant.getName()))) {
-								int power = ce.getLevel(item, ce.getEnchantmentFromName(enchant.getName()));
-								if(players.size() > 0) {
-									for(Player other : players) {
-										Bukkit.getPluginManager().callEvent(new AuraActiveEvent(player, other, enchant, power));
-									}
+				Player player = e.getPlayer();
+				ArrayList<Player> players = getNearByPlayers(player, 3);
+				for(ItemStack item : player.getEquipment().getArmorContents()) {//The player that moves.
+					List<CEnchantment> enchantments = ce.getEnchantmentsOnItem(item);
+					if(!enchantments.isEmpty()) {
+						for(CEnchantments enchantment : auraEnchantments) {
+							if(enchantments.contains(enchantment.getEnchantment())) {
+								for(Player other : players) {
+									Bukkit.getPluginManager().callEvent(new AuraActiveEvent(player, other, enchantment, ce.getLevel(item, enchantment.getEnchantment())));
 								}
 							}
 						}
@@ -40,11 +47,11 @@ public class AuraListener implements Listener {
 				}
 				for(Player other : players) {
 					for(ItemStack item : other.getEquipment().getArmorContents()) {// The other players moving.
-						if(ce.hasEnchantments(item)) {
-							for(CEnchantments enchant : getAuraEnchantments()) {
-								if(ce.hasEnchantment(item, ce.getEnchantmentFromName(enchant.getName()))) {
-									Bukkit.getPluginManager().callEvent(new AuraActiveEvent(other, e.getPlayer(), enchant,
-									ce.getLevel(item, ce.getEnchantmentFromName(enchant.getName()))));
+						List<CEnchantment> enchantments = ce.getEnchantmentsOnItem(item);
+						if(!enchantments.isEmpty()) {
+							for(CEnchantments enchantment : auraEnchantments) {
+								if(enchantments.contains(enchantment.getEnchantment())) {
+									Bukkit.getPluginManager().callEvent(new AuraActiveEvent(other, player, enchantment, ce.getLevel(item, enchantment.getEnchantment())));
 								}
 							}
 						}
@@ -54,22 +61,12 @@ public class AuraListener implements Listener {
 		}
 	}
 	
-	private ArrayList<CEnchantments> getAuraEnchantments() {
-		ArrayList<CEnchantments> enchants = new ArrayList<>();
-		enchants.add(CEnchantments.BLIZZARD);
-		enchants.add(CEnchantments.ACIDRAIN);
-		enchants.add(CEnchantments.SANDSTORM);
-		enchants.add(CEnchantments.RADIANT);
-		enchants.add(CEnchantments.INTIMIDATE);
-		return enchants;
-	}
-	
 	private ArrayList<Player> getNearByPlayers(Player player, int radius) {
 		ArrayList<Player> players = new ArrayList<>();
-		for(Entity en : player.getNearbyEntities(radius, radius, radius)) {
-			if(en instanceof Player) {
-				if(en != player) {
-					players.add((Player) en);
+		for(Entity entity : player.getNearbyEntities(radius, radius, radius)) {
+			if(entity instanceof Player) {
+				if(entity != player) {
+					players.add((Player) entity);
 				}
 			}
 		}
