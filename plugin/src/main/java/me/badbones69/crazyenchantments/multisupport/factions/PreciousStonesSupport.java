16101a1589aadfeb9578c68aee7917154fe1e5ce
BadBones69 <joewojcik14@gmail.com>
2020-03-19 01:47:46 -0400

    Updated some dependencies.

--- plugin/src/main/java/me/badbones69/crazyenchantments/multisupport/factions/PreciousStonesSupport.java
+++ plugin/src/main/java/me/badbones69/crazyenchantments/multisupport/factions/PreciousStonesSupport.java
@@ -1,18 +1,14 @@
 package me.badbones69.crazyenchantments.multisupport.factions;
 
 import net.sacredlabyrinth.Phaed.PreciousStones.PreciousStones;
-import net.sacredlabyrinth.Phaed.PreciousStones.api.IApi;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
 public class PreciousStonesSupport {
     
-    private static IApi preciousStones = PreciousStones.API();
-    
     //No api to check if the player is in their land.
-    
     public static boolean canBreakBlock(Player player, Block block) {
-        return preciousStones.canBreak(player, block.getLocation());
+        return PreciousStones.API().canBreak(player, block.getLocation());
     }
     
 }
