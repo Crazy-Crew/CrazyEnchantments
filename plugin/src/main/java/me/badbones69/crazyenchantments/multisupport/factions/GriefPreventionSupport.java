e068a329828562f8f05fdda4cc4de400e879908d
BadBones69 <joewojcik14@gmail.com>
2020-03-19 01:47:22 -0400

    Fixed: #483
    Fixed:
    - NPE when hooking into GriefProtection.

--- plugin/src/main/java/me/badbones69/crazyenchantments/multisupport/factions/GriefPreventionSupport.java
+++ plugin/src/main/java/me/badbones69/crazyenchantments/multisupport/factions/GriefPreventionSupport.java
@@ -7,20 +7,18 @@ import org.bukkit.entity.Player;
 
 public class GriefPreventionSupport implements FactionPlugin {
     
-    private static GriefPrevention gp = GriefPrevention.instance;
-    
     public boolean inTerritory(Player player) {
-        Claim claim = gp.dataStore.getClaimAt(player.getLocation(), true, null);
+        Claim claim = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), true, null);
         return claim != null && (claim.getOwnerName().equalsIgnoreCase(player.getName()) || claim.allowAccess(player) == null);
     }
     
     public boolean isFriendly(Player player, Player other) {
-        Claim claim = gp.dataStore.getClaimAt(player.getLocation(), true, null);
+        Claim claim = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), true, null);
         return claim != null && claim.allowAccess(other) == null;
     }
     
     public boolean canBreakBlock(Player player, Block block) {
-        Claim claim = gp.dataStore.getClaimAt(block.getLocation(), true, null);
+        Claim claim = GriefPrevention.instance.dataStore.getClaimAt(block.getLocation(), true, null);
         return claim == null || claim.allowEdit(player) == null;
     }
     
