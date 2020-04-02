e80c10a0157274637ec4b55ef52987a4d49ac089
BadBones69 <joewojcik14@gmail.com>
2020-04-01 22:18:34 -0400

    Updated Towny hook to 95.2.0

--- plugin/src/main/java/me/badbones69/crazyenchantments/multisupport/factions/TownySupport.java
+++ plugin/src/main/java/me/badbones69/crazyenchantments/multisupport/factions/TownySupport.java
@@ -1,10 +1,12 @@
 package me.badbones69.crazyenchantments.multisupport.factions;
 
+import com.palmergames.bukkit.towny.Towny;
+import com.palmergames.bukkit.towny.TownyAPI;
 import com.palmergames.bukkit.towny.exceptions.NotRegisteredException;
 import com.palmergames.bukkit.towny.object.Resident;
-import com.palmergames.bukkit.towny.object.TownBlock;
-import com.palmergames.bukkit.towny.object.TownyUniverse;
-import com.palmergames.bukkit.towny.object.WorldCoord;
+import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.utils.CombatUtil;
+import me.badbones69.crazyenchantments.multisupport.Support.SupportedPlugins;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 
@@ -12,9 +14,9 @@ public class TownySupport implements FactionPlugin {
     
     public boolean inTerritory(Player player) {
         try {
-            TownBlock block = WorldCoord.parseWorldCoord(player).getTownBlock();
-            Resident playerUser = TownyUniverse.getDataSource().getResident(player.getName());
-            if (playerUser.hasTown() && playerUser.getTown().hasTownBlock(block)) {
+            Town town = TownyAPI.getInstance().getTownBlock(player.getLocation()).getTown();
+            Resident resident = TownyAPI.getInstance().getDataSource().getResident(player.getName());
+            if (resident.hasTown() && resident.getTown().equals(town)) {
                 return true;
             }
         } catch (NotRegisteredException ignored) {
@@ -23,15 +25,7 @@ public class TownySupport implements FactionPlugin {
     }
     
     public boolean isFriendly(Player player, Player other) {
-        try {
-            Resident playerUser = TownyUniverse.getDataSource().getResident(player.getName());
-            Resident otherUser = TownyUniverse.getDataSource().getResident(other.getName());
-            if (playerUser.hasTown() && otherUser.hasTown() && playerUser.getTown().getName().equalsIgnoreCase(otherUser.getTown().getName())) {
-                return true;
-            }
-        } catch (NotRegisteredException ignored) {
-        }
-        return false;
+        return CombatUtil.preventDamageCall((Towny) SupportedPlugins.TOWNY.getPlugin(), player, other);
     }
     
     public boolean canBreakBlock(Player player, Block block) {
