e704b928fb8c858485c3b0f19d5bd9a7847f9600
BadBones69 <joewojcik14@gmail.com>
2020-08-10 23:06:25 -0400

    Fixed NPE from BlackSmithManager.

--- plugin/src/main/java/me/badbones69/crazyenchantments/api/managers/BlackSmithManager.java
+++ plugin/src/main/java/me/badbones69/crazyenchantments/api/managers/BlackSmithManager.java
@@ -31,18 +31,18 @@ public class BlackSmithManager {
         FileConfiguration config = Files.CONFIG.getFile();
         denyBarrier = new ItemBuilder()
         .setMaterial(Material.BARRIER)
-        .setName(config.getString("Settings.BlackSmith.Results.None"))
+        .setName(config.getString("Settings.BlackSmith.Results.None", "&c&lNo Results"))
         .setLore(config.getStringList("Settings.BlackSmith.Results.Not-Found-Lore"))
         .build();
         redGlass = new ItemBuilder().setMaterial("RED_STAINED_GLASS_PANE", "STAINED_GLASS_PANE:14").setName(" ").build();
         grayGlass = new ItemBuilder().setMaterial("GRAY_STAINED_GLASS_PANE", "STAINED_GLASS_PANE:7").setName(" ").build();
         blueGlass = new ItemBuilder().setMaterial("LIGHT_BLUE_STAINED_GLASS_PANE", "STAINED_GLASS_PANE:3").build();
-        menuName = Methods.color(config.getString("Settings.BlackSmith.GUIName"));
-        foundString = config.getString("Settings.BlackSmith.Results.Found");
-        currency = Currency.getCurrency(config.getString("Settings.BlackSmith.Transaction.Currency"));
-        bookUpgrade = config.getInt("Settings.BlackSmith.Transaction.Costs.Book-Upgrade");
-        levelUp = config.getInt("Settings.BlackSmith.Transaction.Costs.Power-Up");
-        addEnchantment = config.getInt("Settings.BlackSmith.Transaction.Costs.Add-Enchantment");
+        menuName = Methods.color(config.getString("Settings.BlackSmith.GUIName", "&7&lThe &b&lBlack &9&lSmith"));
+        foundString = config.getString("Settings.BlackSmith.Results.Found", "&c&lCost: &6&l%cost%XP");
+        currency = Currency.getCurrency(config.getString("Settings.BlackSmith.Transaction.Currency", "XP_Level"));
+        bookUpgrade = config.getInt("Settings.BlackSmith.Transaction.Costs.Book-Upgrade", 5);
+        levelUp = config.getInt("Settings.BlackSmith.Transaction.Costs.Power-Up", 5);
+        addEnchantment = config.getInt("Settings.BlackSmith.Transaction.Costs.Add-Enchantment", 3);
         maxEnchantments = config.getBoolean("Settings.EnchantmentOptions.MaxAmountOfEnchantmentsToggle");
     }
     
