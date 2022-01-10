e423cb2bd931875cebff8f0d0534073237b72f76
BadBones <joewojcik14@gmail.com>
2022-01-09 19:23:52 -0500

    Update Poms and PlotSquared

--- v1_13_up/src/main/java/me/badbones69/crazyenchantments/multisupport/plotsquared/PlotSquared.java
+++ v1_13_up/src/main/java/me/badbones69/crazyenchantments/multisupport/plotsquared/PlotSquared.java
@@ -1,6 +1,5 @@
 package me.badbones69.crazyenchantments.multisupport.plotsquared;
 
-import com.plotsquared.bukkit.util.BukkitUtil;
 import com.plotsquared.core.player.PlotPlayer;
 import com.plotsquared.core.plot.Plot;
 import org.bukkit.entity.Player;
@@ -8,7 +7,7 @@ import org.bukkit.entity.Player;
 public class PlotSquared implements PlotSquaredVersion {
     
     public boolean inTerritory(Player player) {
-        PlotPlayer<Player> plotPlayer = BukkitUtil.adapt(player);
+        PlotPlayer<Player> plotPlayer = PlotPlayer.from(player);
         Plot plot = plotPlayer.getCurrentPlot();
         if (plot == null) {
             return false;
