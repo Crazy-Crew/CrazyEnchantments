90b9abdf6d5e32e749ab1a8ac269a0a58ad2a90f
Ryder Belserion <github@corecraft.me>
2022-07-06 21:17:24 -0400

    Disable metrics if they choose to.

--- src/main/java/com/badbones69/crazyenchantments/CrazyEnchantments.kt
+++ src/main/java/com/badbones69/crazyenchantments/CrazyEnchantments.kt
@@ -6,9 +6,39 @@ import com.badbones69.crazyenchantments.api.FileManager.Files
 import com.badbones69.crazyenchantments.api.PluginSupport.SupportedPlugins
 import com.badbones69.crazyenchantments.api.economy.CurrencyAPI
 import com.badbones69.crazyenchantments.api.multisupport.misc.spawners.SilkSpawnerSupport
-import com.badbones69.crazyenchantments.commands.*
-import com.badbones69.crazyenchantments.controllers.*
-import com.badbones69.crazyenchantments.enchantments.*
+import com.badbones69.crazyenchantments.commands.BlackSmithCommand
+import com.badbones69.crazyenchantments.commands.CECommand
+import com.badbones69.crazyenchantments.commands.CETab
+import com.badbones69.crazyenchantments.commands.GkitzCommand
+import com.badbones69.crazyenchantments.commands.GkitzTab
+import com.badbones69.crazyenchantments.commands.TinkerCommand
+import com.badbones69.crazyenchantments.controllers.ArmorListener
+import com.badbones69.crazyenchantments.controllers.AuraListener
+import com.badbones69.crazyenchantments.controllers.BlackSmith
+import com.badbones69.crazyenchantments.controllers.CommandChecker
+import com.badbones69.crazyenchantments.controllers.DustControl
+import com.badbones69.crazyenchantments.controllers.EnchantmentControl
+import com.badbones69.crazyenchantments.controllers.FireworkDamage
+import com.badbones69.crazyenchantments.controllers.GKitzController
+import com.badbones69.crazyenchantments.controllers.InfoGUIControl
+import com.badbones69.crazyenchantments.controllers.LostBookController
+import com.badbones69.crazyenchantments.controllers.ProtectionCrystal
+import com.badbones69.crazyenchantments.controllers.Scrambler
+import com.badbones69.crazyenchantments.controllers.ScrollControl
+import com.badbones69.crazyenchantments.controllers.ShopControl
+import com.badbones69.crazyenchantments.controllers.SignControl
+import com.badbones69.crazyenchantments.controllers.Tinkerer
+import com.badbones69.crazyenchantments.enchantments.AllyEnchantments
+import com.badbones69.crazyenchantments.enchantments.Armor
+import com.badbones69.crazyenchantments.enchantments.Axes
+import com.badbones69.crazyenchantments.enchantments.Boots
+import com.badbones69.crazyenchantments.enchantments.Bows
+import com.badbones69.crazyenchantments.enchantments.Helmets
+import com.badbones69.crazyenchantments.enchantments.Hoes
+import com.badbones69.crazyenchantments.enchantments.PickAxes
+import com.badbones69.crazyenchantments.enchantments.Swords
+import com.badbones69.crazyenchantments.enchantments.Tools
+import org.bstats.bukkit.Metrics
 import org.bukkit.attribute.Attribute
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -16,10 +46,9 @@ import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-
 class CrazyEnchantments : JavaPlugin(), Listener {
 
-    private val manager = CrazyManager.getInstance()
+    private val crazyManager = CrazyManager.getInstance()
 
     private val fileManager = FileManager.getInstance()
 
@@ -31,9 +60,24 @@ class CrazyEnchantments : JavaPlugin(), Listener {
     private val generic = Attribute.GENERIC_MAX_HEALTH
 
     override fun onEnable() {
-        fileManager.logInfo(true).setup(plugin)
+        crazyManager.loadPlugin(plugin)
 
-        manager.load()
+        fileManager.logInfo(true).setup()
+
+        val metricsEnabled = Files.CONFIG.file.getBoolean("Settings.Toggle-Metrics")
+
+        if (Files.CONFIG.file.getString("Settings.Toggle-Metrics") != null) {
+            if (metricsEnabled) Metrics(plugin, 4494)
+        } else {
+            logger.warning("Metrics was automatically enabled.")
+            logger.warning("Please add Toggle-Metrics: false to the top of your config.yml")
+            logger.warning("https://github.com/Crazy-Crew/Crazy-Crates/blob/main/src/main/resources/config.yml")
+            logger.warning("An example if confused is linked above.")
+
+            Metrics(plugin, 4494)
+        }
+
+        crazyManager.load()
 
         SupportedPlugins.printHooks()
 
@@ -42,12 +86,13 @@ class CrazyEnchantments : JavaPlugin(), Listener {
         val patchHealth = Files.CONFIG.file.getBoolean("Settings.Reset-Players-Max-Health")
 
         plugin.server.onlinePlayers.forEach {
-            manager.loadCEPlayer(it)
+            crazyManager.loadCEPlayer(it)
+
             if (patchHealth) it.getAttribute(generic)?.baseValue = it.getAttribute(generic)?.baseValue!!
         }
 
         plugin.server.scheduler.runTaskTimerAsynchronously(plugin, Runnable {
-            manager.cePlayers.forEach { manager.backupCEPlayer(it) }
+            crazyManager.cePlayers.forEach { crazyManager.backupCEPlayer(it) }
         }, 5 * 20 * 60, 5 * 20 * 60)
 
         enable()
@@ -90,7 +135,7 @@ class CrazyEnchantments : JavaPlugin(), Listener {
             server.pluginManager.registerEvents(loop, plugin)
         }
 
-        if (manager.isGkitzEnabled) {
+        if (crazyManager.isGkitzEnabled) {
             logger.info("Gkitz support is now enabled.")
             server.pluginManager.registerEvents(GKitzController(), plugin)
         }
@@ -111,16 +156,16 @@ class CrazyEnchantments : JavaPlugin(), Listener {
     private fun disable() {
         armor?.stop()
 
-        if (manager.allyManager != null) manager.allyManager.forceRemoveAllies()
+        if (crazyManager.allyManager != null) crazyManager.allyManager.forceRemoveAllies()
 
-        server.onlinePlayers.forEach { manager.unloadCEPlayer(it) }
+        server.onlinePlayers.forEach { crazyManager.unloadCEPlayer(it) }
     }
 
     @EventHandler
     fun onPlayerJoin(e: PlayerJoinEvent) {
         val player = e.player
-        manager.loadCEPlayer(player)
-        manager.updatePlayerEffects(player)
+        crazyManager.loadCEPlayer(player)
+        crazyManager.updatePlayerEffects(player)
 
         val patchHealth = Files.CONFIG.file.getBoolean("Settings.Reset-Players-Max-Health")
 
@@ -129,8 +174,6 @@ class CrazyEnchantments : JavaPlugin(), Listener {
 
     @EventHandler
     fun onPlayerQuit(e: PlayerQuitEvent) {
-        manager.unloadCEPlayer(e.player)
+        crazyManager.unloadCEPlayer(e.player)
     }
 }
-
-fun getPlugin() = JavaPlugin.getPlugin(CrazyEnchantments::class.java)
