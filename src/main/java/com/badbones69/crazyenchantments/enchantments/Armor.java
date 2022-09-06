f381ab57330a8ec6054489ebcdd627e34e303316
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-09-05 22:57:09 -0400

    Fix class not found

--- src/main/java/com/badbones69/crazyenchantments/enchantments/Armor.java
+++ src/main/java/com/badbones69/crazyenchantments/enchantments/Armor.java
@@ -28,6 +28,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
@@ -101,7 +102,7 @@ public class Armor implements Listener {
     // Ryder start
     @EventHandler
     public void onArmorRightClickEquip(PlayerInteractEvent event) {
-        if (!event.getAction().isRightClick()) return;
+        if (!(event.getAction() == Action.RIGHT_CLICK_AIR) || !(event.getAction() == Action.RIGHT_CLICK_BLOCK)) return;
 
         Player player = event.getPlayer();
 
