740c4e359898fa834e5dcd05fa6d4c996276d9de
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-10-10 21:23:33 -0400

    Disable spartan support temporarily

--- src/main/java/com/badbones69/crazyenchantments/api/multisupport/interfaces/anticheats/SpartanVersion.java
+++
@@ -1,9 +0,0 @@
-package com.badbones69.crazyenchantments.api.multisupport.interfaces.anticheats;
-
-import org.bukkit.entity.Player;
-
-public interface SpartanVersion {
-
-    void cancelFastBreak(Player player);
-
-}
