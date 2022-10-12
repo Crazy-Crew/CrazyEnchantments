2a7c92232214b23e3ee8eccada518f4257d6e692
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2022-10-11 23:39:39 -0400

    Forgot bracket

--- build.gradle.kts
+++ build.gradle.kts
@@ -137,7 +137,7 @@ java {
 tasks {
     shadowJar {
         if (buildNumber != null) {
-            archiveFileName.set("${rootProject.name}-[v${jenkinsVersion}.jar")
+            archiveFileName.set("${rootProject.name}-[v${jenkinsVersion}].jar")
         } else {
             archiveFileName.set("${rootProject.name}-[v${rootProject.version}].jar")
         }
