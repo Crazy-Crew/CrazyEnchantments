f87949656c5d5587f1bae23c666d3c98a34cf7a7
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2023-02-06 13:20:16 -0500

    [ci-skip] Use List.of instead of Array.asList

--- platforms/paper/src/main/java/com/badbones69/crazyenchantments/api/managers/BowEnchantmentManager.java
+++ platforms/paper/src/main/java/com/badbones69/crazyenchantments/api/managers/BowEnchantmentManager.java
@@ -5,7 +5,6 @@ import com.badbones69.crazyenchantments.api.objects.BowEnchantment;
 import com.badbones69.crazyenchantments.api.objects.PotionEffects;
 import org.bukkit.potion.PotionEffectType;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class BowEnchantmentManager {
@@ -17,11 +16,11 @@ public class BowEnchantmentManager {
 
         if (CEnchantments.PIERCING.isActivated()) bowEnchantments.add(new BowEnchantment(CEnchantments.PIERCING, 2, false));
 
-        if (CEnchantments.ICEFREEZE.isActivated()) bowEnchantments.add(new BowEnchantment(CEnchantments.ICEFREEZE, Arrays.asList(new PotionEffects(PotionEffectType.SLOW, 5 * 20, 1)), false));
+        if (CEnchantments.ICEFREEZE.isActivated()) bowEnchantments.add(new BowEnchantment(CEnchantments.ICEFREEZE, List.of(new PotionEffects(PotionEffectType.SLOW, 5 * 20, 1)), false));
 
-        if (CEnchantments.VENOM.isActivated()) bowEnchantments.add(new BowEnchantment(CEnchantments.VENOM, Arrays.asList(new PotionEffects(PotionEffectType.POISON, 2 * 20, -1)), true));
+        if (CEnchantments.VENOM.isActivated()) bowEnchantments.add(new BowEnchantment(CEnchantments.VENOM, List.of(new PotionEffects(PotionEffectType.POISON, 2 * 20, -1)), true));
 
-        if (CEnchantments.SNIPER.isActivated()) bowEnchantments.add(new BowEnchantment(CEnchantments.SNIPER, Arrays.asList(new PotionEffects(PotionEffectType.POISON, 5 * 20, 1)), false));
+        if (CEnchantments.SNIPER.isActivated()) bowEnchantments.add(new BowEnchantment(CEnchantments.SNIPER, List.of(new PotionEffects(PotionEffectType.POISON, 5 * 20, 1)), false));
     }
     
     public List<BowEnchantment> getBowEnchantments() {
