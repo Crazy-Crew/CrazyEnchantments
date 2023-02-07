5e9b73dff169f5a6db65027b8946f5bc1fb4ef3b
Ryder Belserion <52214219+RyderBelserion@users.noreply.github.com>
2023-02-06 23:16:53 -0500

    Update grief prevention api

--- platforms/paper/src/main/java/com/badbones69/crazyenchantments/api/support/claims/GriefPreventionSupport.java
+++ platforms/paper/src/main/java/com/badbones69/crazyenchantments/api/support/claims/GriefPreventionSupport.java
@@ -2,6 +2,7 @@ package com.badbones69.crazyenchantments.api.support.claims;
 
 import com.badbones69.crazyenchantments.api.support.interfaces.claims.FactionsVersion;
 import me.ryanhamshire.GriefPrevention.Claim;
+import me.ryanhamshire.GriefPrevention.ClaimPermission;
 import me.ryanhamshire.GriefPrevention.GriefPrevention;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -12,20 +13,20 @@ public class GriefPreventionSupport implements FactionsVersion {
     public boolean isFriendly(Player player, Player other) {
         Claim claim = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), true, null);
 
-        return claim != null && claim.allowAccess(other) == null;
+        return claim != null && claim.hasExplicitPermission(player.getUniqueId(), ClaimPermission.Access);
     }
 
     @Override
     public boolean inTerritory(Player player) {
         Claim claim = GriefPrevention.instance.dataStore.getClaimAt(player.getLocation(), true, null);
 
-        return claim != null && (claim.getOwnerName().equalsIgnoreCase(player.getName()) || claim.allowAccess(player) == null);
+        return claim != null && (claim.getOwnerID().equals(player.getUniqueId()) || claim.hasExplicitPermission(player.getUniqueId(), ClaimPermission.Access));
     }
 
     @Override
     public boolean canBreakBlock(Player player, Block block) {
         Claim claim = GriefPrevention.instance.dataStore.getClaimAt(block.getLocation(), true, null);
 
-        return claim == null || claim.allowEdit(player) == null;
+        return claim == null || claim.hasExplicitPermission(player.getUniqueId(), ClaimPermission.Build) || claim.hasExplicitPermission(player.getUniqueId(), ClaimPermission.Edit);
     }
 }
