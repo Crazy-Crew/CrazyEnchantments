ca6bb0e21842ad6e98d314004f2affdb6acea80f
Ryder Belserion <no-reply@ryderbelserion.com>
2023-04-08 00:40:49 -0400

    Revert "Lets see if it works"
    This reverts commit d5db4a36f5d106ac6a2f89009fd7b5a322104f86.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,21 +20,11 @@ val repo = if (beta) "beta" else "releases"
 
 val url = if (beta) "https://ci.crazycrew.us/job/${rootProject.name}/" else "https://modrinth.com/$extension/${rootProject.name.lowercase()}/versions"
 val download = if (beta) "https://ci.crazycrew.us/job/${rootProject.name}/" else "https://modrinth.com/$extension/${rootProject.name.lowercase()}/version/${rootProject.version}"
-
-val desc = if (beta) """
-    Changes:
-    » N/A
-""".trimIndent() else "https://modrinth.com/$extension/${rootProject.name.lowercase()}/version/${rootProject.version}"
-
-val msg = if (beta) "New version of ${rootProject.name} is ready!" else "New version of ${rootProject.name} is ready!"
+val msg = if (beta) "New version of ${rootProject.name} is ready!" else "New version of ${rootProject.name} is ready! <@&1029922295210311681>"
 
 val hash = shellRun("git", listOf("rev-parse", "--short", "HEAD"))
 
 rootProject.version = if (beta) hash else "1.9.8.2"
-
-tasks.named("build") {
-    dependsOn("publish", "webhook")
-}
 
 webhook {
     this.avatar("https://en.gravatar.com/avatar/${WebhookExtension.Gravatar().md5Hex("no-reply@ryderbelserion.com")}.jpeg")
@@ -71,7 +61,10 @@ webhook {
 
             this.title("What changed?")
 
-            this.description(desc)
+            this.description("""
+                Changes:
+                » N/A
+            """.trimIndent())
         }
     }
 
