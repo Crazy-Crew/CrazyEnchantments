f7ba154c4b1686316c55a3ef1e59711c169272b7
TrueDarkLord <TrueDarkLord.Email@Gmail.com>
2023-08-11 19:53:48 -0600

    Change Log

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,25 @@ tasks {
 
 val description = """
 
+## New Enchantments:
+|Enchant | Type | Description|
+|---------|-------|-------------|
+DemonForged  | Axe | Deal more durability damage.
+Crouch | Armor | While sneaking, take less damage.
+SystemReboot | Armor | An attack that would normally kill you can instead heal you to full HP.
+Adrenaline | Boots | When on low HP, chance to get a huge speed boost.
+ShockWave | Armor | Chance to Knockback Enemy when they damage you.
+Maneuver | Armor | Chance to dodge an attack.
+VeinMiner | Pickaxe | For each level, will mine 1 more block from ore veins.
+
+## Config Options Added:
+* `VeinMiner-Full-Durability: true`
+* `Drop-Blocks-For-VeinMiner: true`
+
+## Misc:
+* New Default Category added for the new enchants. `Mythical`
+* Updated default CrazyEnchantment colour.
+
 ## Fix:
 * Fixed tinkerer scaling not working properly for books.
 
