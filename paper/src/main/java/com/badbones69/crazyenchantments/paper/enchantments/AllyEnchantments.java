cee615df688559b9e935cd694b00375d8d1d282b
TrueDarkLord <TrueDarkLord.Email@Gmail.com>
2023-11-13 16:55:43 -0700

    Misc Ally Changes
    #753

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/enchantments/AllyEnchantments.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/enchantments/AllyEnchantments.java
@@ -2,11 +2,11 @@ package com.badbones69.crazyenchantments.paper.enchantments;
 
 import com.badbones69.crazyenchantments.paper.CrazyEnchantments;
 import com.badbones69.crazyenchantments.paper.Starter;
-import com.badbones69.crazyenchantments.paper.api.CrazyManager;
 import com.badbones69.crazyenchantments.paper.api.enums.CEnchantments;
 import com.badbones69.crazyenchantments.paper.api.managers.AllyManager;
 import com.badbones69.crazyenchantments.paper.api.objects.AllyMob;
 import com.badbones69.crazyenchantments.paper.api.objects.AllyMob.AllyType;
+import com.badbones69.crazyenchantments.paper.api.objects.CEnchantment;
 import com.badbones69.crazyenchantments.paper.controllers.settings.EnchantmentBookSettings;
 import com.badbones69.crazyenchantments.paper.controllers.settings.EnchantmentSettings;
 import com.badbones69.crazyenchantments.paper.utilities.misc.EventUtils;
@@ -21,8 +21,10 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.EntityTargetEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
-import org.bukkit.inventory.ItemStack;
+
+import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Map;
 
 public class AllyEnchantments implements Listener {
 
@@ -30,12 +32,10 @@ public class AllyEnchantments implements Listener {
 
     private final Starter starter = plugin.getStarter();
 
-    private final CrazyManager crazyManager = starter.getCrazyManager();
-
     // Settings.
     private final EnchantmentSettings enchantmentSettings = starter.getEnchantmentSettings();
 
-    private final EnchantmentBookSettings enchantmentBookSettings = starter.getEnchantmentBookSettings();
+    private final EnchantmentBookSettings bookSettings = starter.getEnchantmentBookSettings();
 
     // Plugin Managers.
     private final AllyManager allyManager = starter.getAllyManager();
@@ -46,19 +46,8 @@ public class AllyEnchantments implements Listener {
 
         if (event.getEntity() instanceof Player player && event.getDamager() instanceof LivingEntity enemy) { // Player gets attacked
             if (!inCoolDown(player)) {
-                for (ItemStack item : player.getEquipment().getArmorContents()) {
-                    // Spawn allies when getting attacked.
-                    if (enchantmentBookSettings.hasEnchantments(item) && enemy instanceof Player) {
-
-                        checkAllyType(enemy, player, item);
-
-                        if (crazyManager.hasEnchantment(item, CEnchantments.BEEKEEPER)) {
-                            int power = crazyManager.getLevel(item, CEnchantments.BEEKEEPER);
-                            spawnAllies(player, enemy, AllyType.BEE, power);
-                        }
-
-                        checkEnchantment(enemy, player, item);
-                    }
+                if (enemy instanceof Player) { // Checks armor and spawn allies when getting attacked.
+                    Arrays.stream(player.getEquipment().getArmorContents()).map(bookSettings::getEnchantments).filter(enchants -> !enchants.isEmpty()).forEach(enchants -> checkAllyType(enemy, player, enchants));
                 }
             } else {
                 allyManager.setEnemy(player, enemy);
@@ -71,44 +60,39 @@ public class AllyEnchantments implements Listener {
                 event.setCancelled(true);
                 return;
             }
-
             if (inCoolDown(player)) {
                 allyManager.setEnemy(player, enemy);
                 return;
             }
-
-            for (ItemStack item : player.getEquipment().getArmorContents()) {
-                // Spawn allies when attacking
-                if (enchantmentBookSettings.hasEnchantments(item)) {
-                    checkAllyType(enemy, player, item);
-
-                    if (enemy instanceof Player) checkEnchantment(enemy, player, item);
-                }
-            }
+            // Checks armor and spawns allies when attacking
+            Arrays.stream(player.getEquipment().getArmorContents()).map(bookSettings::getEnchantments).filter(enchants -> !enchants.isEmpty()).forEach(enchants -> checkAllyType(enemy, player, enchants));
         }
     }
 
-    private void checkAllyType(LivingEntity enemy, Player player, ItemStack item) {
-        if (crazyManager.hasEnchantment(item, CEnchantments.TAMER)) {
-            int power = crazyManager.getLevel(item, CEnchantments.TAMER);
+    private void checkAllyType(LivingEntity enemy, Player player, Map<CEnchantment, Integer> enchants) {
+        if (enchants.containsKey(CEnchantments.TAMER.getEnchantment())) {
+            int power = enchants.get(CEnchantments.TAMER.getEnchantment());
             spawnAllies(player, enemy, AllyType.WOLF, power);
         }
 
-        if (crazyManager.hasEnchantment(item, CEnchantments.GUARDS)) {
-            int power = crazyManager.getLevel(item, CEnchantments.GUARDS);
+        if (enchants.containsKey(CEnchantments.GUARDS.getEnchantment())) {
+            int power = enchants.get(CEnchantments.GUARDS.getEnchantment());
             spawnAllies(player, enemy, AllyType.IRON_GOLEM, power);
         }
-    }
 
-    private void checkEnchantment(LivingEntity enemy, Player player, ItemStack item) {
-        if (crazyManager.hasEnchantment(item, CEnchantments.NECROMANCER)) {
-            int power = crazyManager.getLevel(item, CEnchantments.NECROMANCER);
+        if (enchants.containsKey(CEnchantments.BEEKEEPER.getEnchantment())) {
+            int power = enchants.get(CEnchantments.BEEKEEPER.getEnchantment());
+            spawnAllies(player, enemy, AllyType.BEE, power);
+        }
+
+        if (enchants.containsKey(CEnchantments.NECROMANCER.getEnchantment())) {
+            int power = enchants.get(CEnchantments.NECROMANCER.getEnchantment());
 
             spawnAllies(player, enemy, AllyType.ZOMBIE, power * 2);
         }
 
-        if (crazyManager.hasEnchantment(item, CEnchantments.INFESTATION)) {
-            int power = crazyManager.getLevel(item, CEnchantments.INFESTATION);
+        if (enchants.containsKey(CEnchantments.INFESTATION.getEnchantment())) {
+            int power = enchants.get(CEnchantments.INFESTATION.getEnchantment());
 
             spawnAllies(player, enemy, AllyType.ENDERMITE, power * 3);
             spawnAllies(player, enemy, AllyType.SILVERFISH, power * 3);
@@ -117,13 +101,19 @@ public class AllyEnchantments implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
     public void onAllyTarget(EntityTargetEvent event) {
-        if (!(event.getTarget() instanceof Player player)) return;
+        AllyMob allyMob = allyManager.getAllyMob((LivingEntity) event.getEntity());
+        AllyMob target = allyManager.getAllyMob((LivingEntity) event.getTarget());
 
-        if (!(event.getEntity() instanceof LivingEntity entity)) return;
+        // Stop ally mob from attacking other mobs owned by the player.
+        if (allyMob != null && target != null && allyMob.getOwner() == target.getOwner()) {
+            event.setCancelled(true);
+            return;
+        }
 
-        if (!allyManager.isAlly(player, entity)) return;
-
-        event.setCancelled(true);
+        // Stop your pets from targeting you.
+        if (event.getTarget() instanceof Player player && allyMob != null) {
+            if (player == allyMob.getOwner()) event.setCancelled(true);
+        }
     }
 
     @EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
