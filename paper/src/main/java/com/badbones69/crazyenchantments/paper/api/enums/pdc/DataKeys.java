9dcdd3622faacdd28bed572c6cf5cfb8240e045f
TrueDarkLord <TrueDarkLord.Email@Gmail.com>
2023-11-26 17:05:38 -0700

    Slot_Crystal

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/api/enums/pdc/DataKeys.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/api/enums/pdc/DataKeys.java
@@ -15,7 +15,8 @@ public enum DataKeys {
     NO_FIREWORK_DAMAGE("No_Damage", Boolean.class),
     SCROLL("Crazy_Scroll", String.class),
     EXPERIENCE("Experience", String.class),
-    LIMIT_REDUCER("Limit_Reducer", Integer.class);
+    LIMIT_REDUCER("Limit_Reducer", Integer.class),
+    SLOT_CRYSTAL("Slot_Crystal", Boolean.class);
 
     private final static CrazyEnchantments plugin = CrazyEnchantments.getPlugin();
     private final String namSpaceKey;
