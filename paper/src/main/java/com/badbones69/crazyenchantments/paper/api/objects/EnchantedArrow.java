0ccfd7bd772fb3be6616a40dc3567b56fb8ccac7
TrueDarkLord <TrueDarkLord.Email@Gmail.com>
2023-12-16 12:36:38 -0700

    Make it a record

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/api/objects/EnchantedArrow.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/api/objects/EnchantedArrow.java
@@ -1,66 +1,27 @@
 package com.badbones69.crazyenchantments.paper.api.objects;
 
-import com.badbones69.crazyenchantments.paper.CrazyEnchantments;
-import com.badbones69.crazyenchantments.paper.Starter;
-import com.badbones69.crazyenchantments.paper.api.CrazyManager;
 import com.badbones69.crazyenchantments.paper.api.enums.CEnchantments;
-import com.badbones69.crazyenchantments.paper.controllers.settings.EnchantmentBookSettings;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.inventory.ItemStack;
-import java.util.List;
 
-public class EnchantedArrow {
+import java.util.Map;
 
-    private final CrazyEnchantments plugin = CrazyEnchantments.getPlugin();
+public record EnchantedArrow(Arrow arrow, ItemStack bow, Map<CEnchantment, Integer> enchantments) {
 
-    private final Starter starter = plugin.getStarter();
-
-    private final CrazyManager crazyManager = starter.getCrazyManager();
-
-    private final EnchantmentBookSettings enchantmentBookSettings = starter.getEnchantmentBookSettings();
-    
-    private final Arrow arrow;
-    private final ItemStack bow;
-    private final Entity shooter;
-    private final List<CEnchantment> enchantments;
-    
-    public EnchantedArrow(Arrow arrow, Entity shooter, ItemStack bow, List<CEnchantment> enchantments) {
-        this.bow = bow;
-        this.arrow = arrow;
-        this.shooter = shooter;
-        this.enchantments = enchantments;
-    }
-    
-    public Arrow getArrow() {
-        return arrow;
-    }
-    
-    public ItemStack getBow() {
-        return bow;
-    }
-    
     public Entity getShooter() {
-        return shooter;
+        return (Entity) arrow.getShooter();
     }
-    
+
     public int getLevel(CEnchantments enchantment) {
-        return crazyManager.getLevel(bow, enchantment);
+        return enchantments.get(enchantment.getEnchantment());
     }
-    
-    public int getLevel(CEnchantment enchantment) {
-        return enchantmentBookSettings.getLevel(bow, enchantment);
-    }
-    
-    public List<CEnchantment> getEnchantments() {
-        return enchantments;
-    }
-    
+
     public boolean hasEnchantment(CEnchantment enchantment) {
-        return enchantments.contains(enchantment);
+        return enchantments.containsKey(enchantment);
     }
-    
+
     public boolean hasEnchantment(CEnchantments enchantment) {
-        return enchantments.contains(enchantment.getEnchantment());
+        return hasEnchantment(enchantment.getEnchantment());
     }
 }
