dcdeed02035788e26250c2df02f8fc985eae956f
Ryder Belserion <no-reply@ryderbelserion.com>
2024-02-17 01:32:12 -0500

    Copy paste sometimes on auto pilot is not a good idea.

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/api/enums/pdc/DataKeys.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/api/enums/pdc/DataKeys.java
@@ -7,19 +7,19 @@ import org.jetbrains.annotations.NotNull;
 
 public enum DataKeys {
 
-    dust("crazy_dust", PersistentDataType.STRING),
-    white_scroll_protection("white_scroll_protection", PersistentDataType.STRING),
-    enchantments("crazy_enchants", PersistentDataType.STRING),
-    stored_enchantments("stored_enchantments", PersistentDataType.STRING),
-    protection_crystal("is_protections_crystal", PersistentDataType.BOOLEAN),
-    protected_item("is_protected", PersistentDataType.BOOLEAN),
-    scrambler("is_scrambler", PersistentDataType.BOOLEAN),
-    lost_book("lost_book_type", PersistentDataType.STRING),
+    dust("Crazy_Dust", PersistentDataType.STRING),
+    white_scroll_protection("White_Scroll_Protection", PersistentDataType.STRING),
+    enchantments("CrazyEnchants", PersistentDataType.STRING),
+    stored_enchantments("Stored_Enchantments", PersistentDataType.STRING),
+    protection_crystal("is_Protections_Crystal", PersistentDataType.BOOLEAN),
+    protected_item("isProtected", PersistentDataType.BOOLEAN),
+    scrambler("isScrambler", PersistentDataType.BOOLEAN),
+    lost_book("Lost_Book_Type", PersistentDataType.STRING),
     no_firework_damage("no_firework_damage", PersistentDataType.BOOLEAN),
-    scroll("crazy_scroll", PersistentDataType.STRING),
-    experience("experience", PersistentDataType.STRING),
-    limit_reducer("limit_reducer", PersistentDataType.INTEGER),
-    slot_crystal("slot_crystal", PersistentDataType.BOOLEAN);
+    scroll("Crazy_Scroll", PersistentDataType.STRING),
+    experience("Experience", PersistentDataType.STRING),
+    limit_reducer("Limit_Reducer", PersistentDataType.INTEGER),
+    slot_crystal("Slot_Crystal", PersistentDataType.BOOLEAN);
 
     @NotNull
     private final CrazyEnchantments plugin = CrazyEnchantments.get();
@@ -33,7 +33,7 @@ public enum DataKeys {
     }
 
     public NamespacedKey getNamespacedKey() {
-        return new NamespacedKey(this.plugin, this.plugin.getName().toLowerCase() + "_" + this.NamespacedKey);
+        return new NamespacedKey(this.plugin, this.NamespacedKey);
     }
 
     public PersistentDataType getType() {
