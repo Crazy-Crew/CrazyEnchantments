c680b2530b70d99807c45bfe986308bdbb9ef501
Ryder Belserion <no-reply@ryderbelserion.com>
2024-02-19 00:49:15 -0500

    I'll test this code later

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/api/builders/types/blacksmith/BlackSmithMenu.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/api/builders/types/blacksmith/BlackSmithMenu.java
@@ -4,10 +4,14 @@ import com.badbones69.crazyenchantments.paper.CrazyEnchantments;
 import com.badbones69.crazyenchantments.paper.Methods;
 import com.badbones69.crazyenchantments.paper.Starter;
 import com.badbones69.crazyenchantments.paper.api.builders.InventoryBuilder;
+import com.badbones69.crazyenchantments.paper.api.economy.Currency;
+import com.badbones69.crazyenchantments.paper.api.economy.CurrencyAPI;
 import com.badbones69.crazyenchantments.paper.api.enums.Messages;
 import com.badbones69.crazyenchantments.paper.api.objects.BlackSmithResult;
 import com.badbones69.crazyenchantments.paper.controllers.settings.EnchantmentBookSettings;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -59,6 +63,8 @@ public class BlackSmithMenu extends InventoryBuilder {
 
         @NotNull
         private final EnchantmentBookSettings settings = this.starter.getEnchantmentBookSettings();
+
+        private static int[] resultBorder = new int[]{0, 7, 8, 9, 16, 18, 25, 26};
 
         @EventHandler(ignoreCancelled = true)
         public void onInventoryClick(InventoryClickEvent event) {
@@ -120,6 +126,64 @@ public class BlackSmithMenu extends InventoryBuilder {
             }
 
             //todo() add menu clicks.
+            if (event.getRawSlot() == this.mainSlot || event.getRawSlot() == this.subSlot) {
+                event.setCurrentItem(new ItemStack(Material.AIR));
+                this.methods.addItemToInventory(player, item);
+
+                inventory.setItem(this.outputSlot, BlackSmithManager.getExitButton());
+
+                for (int slot : resultBorder) {
+                    inventory.setItem(slot, BlackSmithManager.getBlueGlass());
+                }
+
+                //todo() play sound.
+            }
+
+            if (event.getRawSlot() == this.outputSlot) {
+                if (inventory.getItem(this.mainSlot) != null && inventory.getItem(this.subSlot) != null) {
+                    BlackSmithResult result = new BlackSmithResult(player, inventory.getItem(this.mainSlot), inventory.getItem(this.subSlot));
+
+                    if (result.getCost() > 0) {
+                        Currency currency = BlackSmithManager.getCurrency();
+
+                        if (currency != null && player.getGameMode() != GameMode.CREATIVE) {
+                            CurrencyAPI currencyAPI = this.plugin.getStarter().getCurrencyAPI();
+
+                            if (currencyAPI.canBuy(player, currency, result.getCost())) {
+                                currencyAPI.takeCurrency(player, currency, result.getCost());
+                            } else {
+                                String needed = String.valueOf(result.getCost() - currencyAPI.getCurrency(player, currency));
+
+                                this.methods.switchCurrency(player, currency, "%Money_Needed%", "%XP%", needed);
+                                return;
+                            }
+                        }
+
+                        this.methods.addItemToInventory(player, result.getResultItem());
+
+                        inventory.setItem(this.mainSlot, new ItemStack(Material.AIR));
+                        inventory.setItem(this.subSlot, new ItemStack(Material.AIR));
+
+                        //todo() play sound
+
+                        inventory.setItem(this.outputSlot, BlackSmithManager.getExitButton());
+
+                        for (int slot : resultBorder) {
+                            inventory.setItem(slot, BlackSmithManager.getRedGlass());
+                        }
+                        
+                        return;
+                    }
+
+                    //todo() play sound
+                    player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1f, 1f);
+
+                    return;
+                }
+
+                //todo() play sound
+                player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1f, 1f);
+            }
         }
 
         @EventHandler(ignoreCancelled = true)
@@ -153,15 +217,13 @@ public class BlackSmithMenu extends InventoryBuilder {
         }
 
         private void setBorder(BlackSmithResult item, Inventory inventory) {
-            int[] border = new int[]{0, 7, 8, 9, 16, 18, 25, 26};
-
             if (item.getCost() > 0) {
                 ItemStack result = item.getResultItem();
 
                 String value = String.valueOf(item.getCost());
                 String message = Messages.replacePlaceholders("%Cost%", value, BlackSmithManager.getItemCost());
 
-                for (int slot : border) {
+                for (int slot : resultBorder) {
                     inventory.setItem(slot, BlackSmithManager.getBlueGlass());
                 }
 
@@ -171,7 +233,7 @@ public class BlackSmithMenu extends InventoryBuilder {
 
             inventory.setItem(this.outputSlot, BlackSmithManager.getExitButton());
 
-            for (int slot : border) {
+            for (int slot : resultBorder) {
                 inventory.setItem(slot, BlackSmithManager.getRedGlass());
             }
         }
