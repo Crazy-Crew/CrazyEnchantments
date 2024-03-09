141175ef5efa22d7949a511ce31a2cbd79d807a6
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-09 18:32:31 -0500

    Fix npe

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/api/enums/ShopOption.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/api/enums/ShopOption.java
@@ -6,6 +6,7 @@ import com.badbones69.crazyenchantments.ConfigManager;
 import com.badbones69.crazyenchantments.paper.api.economy.Currency;
 import com.badbones69.crazyenchantments.paper.api.objects.other.ItemBuilder;
 import com.badbones69.crazyenchantments.platform.impl.Config;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
@@ -14,23 +15,23 @@ import java.util.List;
 
 public enum ShopOption {
 
-    GKITZ(Config.gkitz_item, Config.gkitz_name, Config.gkitz_lore, Config.gkitz_glowing, Config.gkitz_in_gui, Config.gkitz_player, Config.gkitz_slot),
-    BLACKSMITH(Config.blacksmith_item, Config.blacksmith_name, Config.blacksmith_lore, Config.blacksmith_glowing, Config.blacksmith_in_gui, Config.blacksmith_player, Config.blacksmith_slot),
-    TINKER(Config.tinker_item, Config.tinker_name, Config.tinker_lore, Config.tinker_glowing, Config.tinker_in_gui, Config.tinker_player, Config.tinker_slot),
-    INFO(Config.info_item, Config.info_name, Config.info_lore, Config.info_glowing, Config.info_in_gui, Config.info_player, Config.info_slot),
+    GKITZ(Config.gkitz_item, Config.gkitz_name, Config.gkitz_lore, Config.gkitz_in_gui, false, Config.gkitz_glowing, Config.scrambler_currency, Config.scrambler_cost, Config.gkitz_slot),
+    BLACKSMITH(Config.blacksmith_item, Config.blacksmith_name, Config.blacksmith_lore, Config.blacksmith_in_gui, false, Config.blacksmith_glowing, Config.scrambler_currency, Config.scrambler_cost, Config.blacksmith_slot),
+    TINKER(Config.tinker_item, Config.tinker_name, Config.tinker_lore, Config.tinker_in_gui, false, Config.tinker_glowing, Config.scrambler_currency, Config.scrambler_cost, Config.tinker_slot),
+    INFO(Config.info_item, Config.info_name, Config.info_lore, Config.info_glowing, false, Config.info_in_gui, Config.scrambler_currency, Config.scrambler_cost, Config.info_slot),
 
-    PROTECTION_CRYSTAL(Config.protection_crystal_item, Config.protection_crystal_gui_name, Config.protection_crystal_gui_lore, Config.protection_crystal_in_gui, true, Config.protection_crystal_glowing, Config.protection_crystal_gui_player, Config.protection_crystal_currency, Config.protection_crystal_cost, Config.protection_crystal_slot),
+    PROTECTION_CRYSTAL(Config.protection_crystal_item, Config.protection_crystal_gui_name, Config.protection_crystal_gui_lore, Config.protection_crystal_in_gui, true, Config.protection_crystal_glowing, Config.protection_crystal_currency, Config.protection_crystal_cost, Config.protection_crystal_slot),
 
     DESTROY_DUST(Config.destroy_dust_item, Config.destroy_dust_gui_name, Config.destroy_dust_gui_lore, Config.destroy_dust_in_gui, true, Config.destroy_dust_currency, Config.destroy_dust_cost, Config.destroy_dust_slot),
     SUCCESS_DUST(Config.success_dust_item, Config.success_dust_gui_name, Config.success_dust_gui_lore, Config.success_dust_in_gui, true, Config.success_dust_currency, Config.success_dust_cost, Config.success_dust_slot),
 
-    SCRAMBLER(Config.scrambler_item, Config.scrambler_gui_name, Config.scrambler_gui_lore, Config.scrambler_in_gui, true, Config.scrambler_glowing, Config.scrambler_gui_player, Config.scrambler_currency, Config.scrambler_cost, Config.scrambler_gui_slot),
+    SCRAMBLER(Config.scrambler_item, Config.scrambler_gui_name, Config.scrambler_gui_lore, Config.scrambler_in_gui, true, Config.scrambler_glowing, Config.scrambler_currency, Config.scrambler_cost, Config.scrambler_gui_slot),
 
-    SLOT_CRYSTAL(Config.slot_crystal_item, Config.slot_crystal_gui_name, Config.slot_crystal_gui_lore, Config.slot_crystal_in_gui, true, Config.slot_crystal_glowing, Config.slot_crystal_gui_player, Config.slot_crystal_currency, Config.slot_crystal_cost, Config.slot_crystal_gui_slot),
+    SLOT_CRYSTAL(Config.slot_crystal_item, Config.slot_crystal_gui_name, Config.slot_crystal_gui_lore, Config.slot_crystal_in_gui, true, Config.slot_crystal_glowing, Config.slot_crystal_currency, Config.slot_crystal_cost, Config.slot_crystal_gui_slot),
 
-    BLACK_SCROLL(Config.black_scroll_item, Config.black_scroll_gui_name, Config.black_scroll_gui_lore, Config.black_scroll_in_gui, true, Config.black_scroll_glowing, Config.black_scroll_gui_player, Config.black_scroll_currency, Config.black_scroll_cost, Config.black_scroll_gui_slot),
-    WHITE_SCROLL(Config.white_scroll_item, Config.white_scroll_gui_name, Config.white_scroll_gui_lore, Config.white_scroll_in_gui, true, Config.white_scroll_glowing, Config.white_scroll_gui_player, Config.white_scroll_currency, Config.white_scroll_cost, Config.white_scroll_gui_slot),
-    TRANSMOG_SCROLL(Config.transmog_scroll_item, Config.transmog_scroll_gui_name, Config.transmog_scroll_gui_lore, Config.transmog_scroll_in_gui, true, Config.transmog_scroll_in_gui, Config.transmog_scroll_gui_player, Config.transmog_currency, Config.transmog_cost, Config.transmog_scroll_gui_slot);
+    BLACK_SCROLL(Config.black_scroll_item, Config.black_scroll_gui_name, Config.black_scroll_gui_lore, Config.black_scroll_in_gui, true, Config.black_scroll_glowing, Config.black_scroll_currency, Config.black_scroll_cost, Config.black_scroll_gui_slot),
+    WHITE_SCROLL(Config.white_scroll_item, Config.white_scroll_gui_name, Config.white_scroll_gui_lore, Config.white_scroll_in_gui, true, Config.white_scroll_glowing, Config.white_scroll_currency, Config.white_scroll_cost, Config.white_scroll_gui_slot),
+    TRANSMOG_SCROLL(Config.transmog_scroll_item, Config.transmog_scroll_gui_name, Config.transmog_scroll_gui_lore, Config.transmog_scroll_in_gui, true, Config.transmog_scroll_in_gui, Config.transmog_currency, Config.transmog_cost, Config.transmog_scroll_gui_slot);
     
     private static final HashMap<ShopOption, Option> shopOptions = new HashMap<>();
 
@@ -40,7 +41,6 @@ public enum ShopOption {
     private final boolean isBuyable;
 
     private final boolean isGlowing;
-    private final String player;
     private String currency;
     private int cost;
     private final int slot;
@@ -50,7 +50,7 @@ public enum ShopOption {
     @NotNull
     private final SettingsManager config = ConfigManager.getConfig();
 
-    ShopOption(Property<String> item, Property<String> name, Property<List<String>> lore, Property<Boolean> inGui, boolean isBuyable, Property<Boolean> isGlowing, Property<String> player, Property<String> currency, Property<Integer> cost, Property<Integer> slot) {
+    ShopOption(Property<String> item, Property<String> name, Property<List<String>> lore, Property<Boolean> inGui, boolean isBuyable, Property<Boolean> isGlowing,Property<String> currency, Property<Integer> cost, Property<Integer> slot) {
         this.material = Material.matchMaterial(this.config.getProperty(item));
 
         this.name = this.config.getProperty(name);
@@ -62,8 +62,6 @@ public enum ShopOption {
         this.inGui = this.config.getProperty(inGui);
 
         this.isGlowing = this.config.getProperty(isGlowing);
-
-        this.player = this.config.getProperty(player);
 
         this.currency = this.config.getProperty(currency);
 
@@ -85,8 +83,6 @@ public enum ShopOption {
 
         this.isGlowing = false;
 
-        this.player = "";
-
         this.slot = this.config.getProperty(slot);
 
         this.currency = this.config.getProperty(currency);
@@ -94,7 +90,7 @@ public enum ShopOption {
         this.cost = this.config.getProperty(cost);
     }
 
-    ShopOption(Property<String> item, Property<String> name, Property<List<String>> lore, Property<Boolean> inGui, Property<Boolean> isGlowing, Property<String> player, Property<Integer> slot) {
+    ShopOption(Property<String> item, Property<String> name, Property<List<String>> lore, Property<Boolean> inGui, Property<Boolean> isGlowing, Property<Integer> slot) {
         this.material = Material.matchMaterial(this.config.getProperty(item));
 
         this.name = this.config.getProperty(name);
@@ -106,8 +102,6 @@ public enum ShopOption {
         this.inGui = this.config.getProperty(inGui);
 
         this.isGlowing = this.config.getProperty(isGlowing);
-
-        this.player = this.config.getProperty(player);
 
         this.slot = this.config.getProperty(slot);
     }
@@ -132,10 +126,6 @@ public enum ShopOption {
         return this.lore;
     }
 
-    public String getPlayer() {
-        return this.player;
-    }
-
     public static void loadShopOptions() {
         shopOptions.clear();
 
@@ -144,7 +134,6 @@ public enum ShopOption {
                     .setName(option.getName())
                     .setLore(option.getLore())
                     .setMaterial(option.getMaterial())
-                    .setPlayerName(option.getPlayer())
                     .setGlow(option.isGlowing());
 
             Option shop = new Option(builder, option.getSlot(), option.isInGui(), option.getCost(), option.getCurrency());
