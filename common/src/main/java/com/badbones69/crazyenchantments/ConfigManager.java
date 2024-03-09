8e0607bb293d957b1bbcaee63d4e9a760a6e3f93
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-09 17:28:12 -0500

    Load tinker config

--- common/src/main/java/com/badbones69/crazyenchantments/ConfigManager.java
+++ common/src/main/java/com/badbones69/crazyenchantments/ConfigManager.java
@@ -3,6 +3,7 @@ package com.badbones69.crazyenchantments;
 import ch.jalu.configme.SettingsManager;
 import ch.jalu.configme.SettingsManagerBuilder;
 import ch.jalu.configme.resource.YamlFileResourceOptions;
+import com.badbones69.crazyenchantments.platform.TinkerConfig;
 import com.badbones69.crazyenchantments.platform.impl.Config;
 import com.badbones69.crazyenchantments.platform.impl.Messages;
 import java.io.File;
@@ -12,6 +13,8 @@ public class ConfigManager {
     private static SettingsManager config;
 
     private static SettingsManager messages;
+
+    private static SettingsManager tinker;
 
     public static void load(File dataFolder) {
         // Create config files
@@ -28,18 +31,28 @@ public class ConfigManager {
                 .useDefaultMigrationService()
                 .configurationData(Messages.class)
                 .create();
+
+        tinker = SettingsManagerBuilder
+                .withYamlFile(new File(dataFolder, "tinker.yml"), builder)
+                .useDefaultMigrationService()
+                .configurationData(TinkerConfig.class)
+                .create();
     }
 
     public static void reload() {
         config.reload();
 
         messages.reload();
+
+        tinker.reload();
     }
 
     public static void disable() {
         config.save();
 
         messages.save();
+
+        tinker.save();
     }
 
     public static SettingsManager getConfig() {
@@ -48,5 +61,9 @@ public class ConfigManager {
 
     public static SettingsManager getMessages() {
         return messages;
+    }
+
+    public static SettingsManager getTinker() {
+        return tinker;
     }
 }
