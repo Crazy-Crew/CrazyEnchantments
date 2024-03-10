4ccc5dc516b5f44c81704b7904b0dc78d02ac8c0
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-09 20:46:00 -0500

    CONFIG option has no usages!

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/api/FileManager.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/api/FileManager.java
@@ -344,7 +344,6 @@ public class FileManager {
         
         // ENUM_NAME("fileName.yml", "fileLocation.yml"),
         // ENUM_NAME("fileName.yml", "newFileLocation.yml", "oldFileLocation.yml"),
-        CONFIG("config.yml", "config.yml"),
         CATEGORIES("Categories.yml", "Categories.yml"),
         HEADMAP("HeadMap.yml", "HeadMap.yml"),
         DATA("Data.yml", "Data.yml"),
