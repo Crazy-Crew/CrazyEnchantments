26c8058f9fcf0209671a1242f9855ab9f6b2c33e
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-26 19:10:27 -0400

    Disable most commands

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/platform/CommandManager.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/platform/CommandManager.java
@@ -1,32 +1,11 @@
-package com.badbones69.crazyenchantments.paper.commands;
+package com.badbones69.crazyenchantments.paper.platform;
 
 import com.badbones69.crazyenchantments.paper.CrazyEnchantments;
 import com.badbones69.crazyenchantments.paper.api.CrazyManager;
 import com.badbones69.crazyenchantments.paper.api.enums.Messages;
 import com.badbones69.crazyenchantments.paper.api.objects.CEnchantment;
 import com.badbones69.crazyenchantments.paper.api.objects.Category;
-import com.badbones69.crazyenchantments.paper.commands.types.CommandHelp;
-import com.badbones69.crazyenchantments.paper.commands.types.CommandInfo;
-import com.badbones69.crazyenchantments.paper.commands.types.CommandLimit;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.CommandAdd;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.CommandBook;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.CommandDust;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.CommandRemove;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.items.CommandLostBook;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.CommandBottle;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.CommandConvert;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.items.CommandCrystal;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.CommandDebug;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.CommandFix;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.CommandGive;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.CommandReload;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.items.CommandScrambler;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.items.CommandScroll;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.items.CommandSlotCrystal;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.CommandSpawn;
-import com.badbones69.crazyenchantments.paper.commands.types.admin.CommandUpdate;
-import com.badbones69.crazyenchantments.paper.commands.types.root.SmithCommand;
-import com.badbones69.crazyenchantments.paper.commands.types.root.TinkerCommand;
+import com.badbones69.crazyenchantments.paper.platform.commands.types.CommandGive;
 import com.badbones69.crazyenchantments.paper.controllers.settings.EnchantmentBookSettings;
 import dev.triumphteam.cmd.bukkit.BukkitCommandManager;
 import dev.triumphteam.cmd.bukkit.message.BukkitMessageKey;
@@ -36,7 +15,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -131,33 +109,34 @@ public class CommandManager {
         });
 
         List.of(
-                new TinkerCommand(),
-                new SmithCommand(),
+                new CommandGive()
 
-                new CommandUpdate(),
-                new CommandLimit(),
-                new CommandInfo(),
-                new CommandHelp(),
+                //new TinkerCommand(),
+                //new SmithCommand(),
 
-                new CommandScroll(),
+                //new CommandUpdate(),
+                //new CommandLimit(),
+                //new CommandInfo(),
+                //new CommandHelp(),
 
-                new CommandAdd(),
-                new CommandBook(),
-                new CommandDust(),
-                new CommandRemove(),
+                //new CommandScroll(),
 
-                new CommandLostBook(),
-                new CommandScrambler(),
-                new CommandSlotCrystal(),
-                new CommandCrystal(),
-                new CommandGive(),
+                //new CommandAdd(),
+                //new CommandBook(),
+                //new CommandDust(),
+                //new CommandRemove(),
 
-                new CommandSpawn(),
-                new CommandConvert(),
-                new CommandBottle(),
-                new CommandReload(),
-                new CommandDebug(),
-                new CommandFix()
+                //new CommandLostBook(),
+                //new CommandScrambler(),
+                //new CommandSlotCrystal(),
+                //new CommandCrystal(),
+
+                //new CommandSpawn(),
+                //new CommandConvert(),
+                //new CommandBottle(),
+                //new CommandReload(),
+                //new CommandDebug(),
+                //new CommandFix()
         ).forEach(getCommandManager()::registerCommand);
     }
 
