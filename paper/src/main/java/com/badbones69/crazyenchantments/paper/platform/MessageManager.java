938848f74bc05c60df7f2b0a35e20fb3cd1ed4c1
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-26 19:09:27 -0400

    Change notnull annotation

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/platform/MessageManager.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/platform/MessageManager.java
@@ -1,4 +1,4 @@
-package com.badbones69.crazyenchantments.paper.commands;
+package com.badbones69.crazyenchantments.paper.platform;
 
 import dev.triumphteam.cmd.bukkit.BukkitCommandManager;
 import org.bukkit.command.CommandSender;
@@ -6,8 +6,7 @@ import org.jetbrains.annotations.NotNull;
 
 public abstract class MessageManager {
 
-    @NotNull
-    protected final BukkitCommandManager<CommandSender> commandManager = CommandManager.getCommandManager();
+    protected final @NotNull BukkitCommandManager<CommandSender> commandManager = CommandManager.getCommandManager();
 
     public abstract void build();
 
