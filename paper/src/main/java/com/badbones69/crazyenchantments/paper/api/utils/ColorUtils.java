46cd2a162c3215b241e835f38da14b66930e1e89
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-26 19:10:33 -0400

    Remove legacy methods

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/api/utils/ColorUtils.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/api/utils/ColorUtils.java
@@ -1,23 +1,14 @@
 package com.badbones69.crazyenchantments.paper.api.utils;
 
-import com.badbones69.crazyenchantments.ConfigManager;
 import com.badbones69.crazyenchantments.paper.api.objects.other.ItemBuilder;
-import com.badbones69.crazyenchantments.platform.impl.Config;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
-import net.md_5.bungee.api.chat.TextComponent;
-import org.bukkit.ChatColor;
 import org.bukkit.Color;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import static java.util.regex.Matcher.quoteReplacement;
 
 public class ColorUtils {
 
@@ -55,55 +46,6 @@ public class ColorUtils {
             case "YELLOW" -> Color.YELLOW;
             default -> Color.WHITE;
         };
-    }
-
-    public static String color(String message) {
-        Matcher matcher = Pattern.compile("#[a-fA-F\\d]{6}").matcher(message);
-        StringBuilder buffer = new StringBuilder();
-
-        while (matcher.find()) {
-            matcher.appendReplacement(buffer, net.md_5.bungee.api.ChatColor.of(matcher.group()).toString());
-        }
-
-        return ChatColor.translateAlternateColorCodes('&', matcher.appendTail(buffer).toString());
-    }
-
-    public static void sendMessage(CommandSender commandSender, String message, boolean prefixToggle) {
-        if (message == null || message.isEmpty()) return;
-
-        String prefix = getPrefix();
-
-        if (commandSender instanceof Player player) {
-            if (!prefix.isEmpty() && prefixToggle)
-                player.sendRichMessage(color(message.replaceAll("%prefix%", quoteReplacement(prefix))).replaceAll("%Prefix%", quoteReplacement(prefix)));
-            else player.sendRichMessage(color(message));
-
-            return;
-        }
-
-        if (!prefix.isEmpty() && prefixToggle)
-            commandSender.sendMessage(color(message.replaceAll("%prefix%", quoteReplacement(prefix))).replaceAll("%Prefix%", quoteReplacement(prefix)));
-        else commandSender.sendMessage(color(message));
-    }
-
-    public static String getPrefix() {
-        return color(ConfigManager.getConfig().getProperty(Config.prefix));
-    }
-
-    public static String getPrefix(String msg) {
-        return color(getPrefix() + msg);
-    }
-
-    public static String sanitizeColor(String msg) {
-        return sanitizeFormat(color(msg));
-    }
-
-    public static String sanitizeFormat(String string) {
-        return TextComponent.toLegacyText(TextComponent.fromLegacyText(string));
-    }
-
-    public static String removeColor(String msg) {
-        return ChatColor.stripColor(msg);
     }
 
     public static net.kyori.adventure.text.TextComponent legacyTranslateColourCodes(String input) {
