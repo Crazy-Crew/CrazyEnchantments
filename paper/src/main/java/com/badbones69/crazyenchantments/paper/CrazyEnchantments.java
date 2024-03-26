016bf07d6d13d952f5c51b5adca507e2cf152c08
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-26 19:10:10 -0400

    Nuke startup class to use only what we need

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/CrazyEnchantments.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/CrazyEnchantments.java
@@ -1,127 +1,44 @@
 package com.badbones69.crazyenchantments.paper;
 
 import com.badbones69.crazyenchantments.ConfigManager;
-import com.badbones69.crazyenchantments.paper.api.builders.types.BaseMenu;
-import com.badbones69.crazyenchantments.paper.api.builders.types.blacksmith.BlackSmithMenu;
-import com.badbones69.crazyenchantments.paper.api.builders.types.gkitz.KitsMenu;
-import com.badbones69.crazyenchantments.paper.api.builders.types.tinkerer.TinkererMenu;
-import com.badbones69.crazyenchantments.paper.commands.CommandManager;
 import com.badbones69.crazyenchantments.paper.controllers.BossBarController;
-import com.badbones69.crazyenchantments.paper.controllers.CommandChecker;
-import com.badbones69.crazyenchantments.paper.controllers.LostBookController;
-import com.badbones69.crazyenchantments.paper.enchantments.AllyEnchantments;
-import com.badbones69.crazyenchantments.paper.enchantments.ArmorEnchantments;
-import com.badbones69.crazyenchantments.paper.enchantments.AxeEnchantments;
-import com.badbones69.crazyenchantments.paper.enchantments.BootEnchantments;
-import com.badbones69.crazyenchantments.paper.enchantments.BowEnchantments;
-import com.badbones69.crazyenchantments.paper.enchantments.HoeEnchantments;
-import com.badbones69.crazyenchantments.paper.enchantments.PickaxeEnchantments;
-import com.badbones69.crazyenchantments.paper.enchantments.SwordEnchantments;
-import com.badbones69.crazyenchantments.paper.enchantments.ToolEnchantments;
-import com.badbones69.crazyenchantments.paper.listeners.AuraListener;
-import com.badbones69.crazyenchantments.paper.listeners.DustControlListener;
-import com.badbones69.crazyenchantments.paper.listeners.FireworkDamageListener;
-import com.badbones69.crazyenchantments.paper.listeners.MiscListener;
-import com.badbones69.crazyenchantments.paper.listeners.ProtectionCrystalListener;
-import com.badbones69.crazyenchantments.paper.listeners.ShopListener;
-import com.badbones69.crazyenchantments.paper.listeners.server.ServerReadyListener;
-import com.badbones69.crazyenchantments.paper.listeners.server.WorldSwitchListener;
-import com.badbones69.crazyenchantments.paper.tasks.MigrationManager;
-import com.badbones69.crazyenchantments.platform.impl.Config;
-import org.bstats.bukkit.Metrics;
+import com.badbones69.crazyenchantments.paper.platform.CommandManager;
+import com.badbones69.crazyenchantments.paper.platform.PaperServer;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CrazyEnchantments extends JavaPlugin {
 
-    // Plugin Listeners.
-    public final PluginManager pluginManager = getServer().getPluginManager();
-    private final BossBarController bossBarController = new BossBarController(this);
+    protected final PluginManager pluginManager = getServer().getPluginManager();
+
+    @Override
+    public void onLoad() {
+        ConfigManager.load(getDataFolder());
+    }
+
     private Starter starter;
-    private ArmorEnchantments armorEnchantments;
 
     @Override
     public void onEnable() {
-        MigrationManager.migrate();
-
-        // Load the configurations
-        ConfigManager.load(getDataFolder());
-
-        // Enable metrics.
-        if (ConfigManager.getConfig().getProperty(Config.toggle_metrics)) new Metrics(this, 4494);
+        new PaperServer(this);
 
         this.starter = new Starter();
         this.starter.run();
 
-        this.starter.getCurrencyAPI().loadCurrency();
-
-        // Load the new commands.
         CommandManager.load();
-
-        // Load what we need to properly enable the plugin.
-        this.starter.getCrazyManager().load();
-
-        // Register the server ready listener
-        this.pluginManager.registerEvents(new ServerReadyListener(), this);
-
-        this.pluginManager.registerEvents(new MiscListener(), this);
-        this.pluginManager.registerEvents(new DustControlListener(), this);
-
-        this.pluginManager.registerEvents(new BlackSmithMenu.BlackSmithListener(), this);
-        this.pluginManager.registerEvents(new KitsMenu.KitsListener(), this);
-        this.pluginManager.registerEvents(new TinkererMenu.TinkererListener(), this);
-        this.pluginManager.registerEvents(new BaseMenu.InfoMenuListener(), this);
-
-        this.pluginManager.registerEvents(new FireworkDamageListener(), this);
-        this.pluginManager.registerEvents(new ShopListener(), this);
-
-        this.pluginManager.registerEvents(new PickaxeEnchantments(), this);
-        this.pluginManager.registerEvents(new SwordEnchantments(), this);
-        this.pluginManager.registerEvents(this.armorEnchantments = new ArmorEnchantments(), this);
-        this.pluginManager.registerEvents(new AllyEnchantments(), this);
-        this.pluginManager.registerEvents(new ToolEnchantments(), this);
-        this.pluginManager.registerEvents(new BootEnchantments(), this);
-        this.pluginManager.registerEvents(new AxeEnchantments(), this);
-        this.pluginManager.registerEvents(new BowEnchantments(), this);
-        this.pluginManager.registerEvents(new HoeEnchantments(), this);
-
-        this.pluginManager.registerEvents(new ProtectionCrystalListener(), this);
-        this.pluginManager.registerEvents(new FireworkDamageListener(), this);
-        this.pluginManager.registerEvents(new AuraListener(), this);
-
-        this.pluginManager.registerEvents(new LostBookController(), this);
-        this.pluginManager.registerEvents(new CommandChecker(), this);
-
-        this.pluginManager.registerEvents(new WorldSwitchListener(), this);
-
-        if (this.starter.getCrazyManager().isGkitzEnabled()) {
-            getLogger().info("G-Kitz support is now enabled.");
-
-            this.pluginManager.registerEvents(new KitsMenu.KitsListener(), this);
-        }
     }
 
     @Override
     public void onDisable() {
-        this.bossBarController.removeAllBossBars();
 
-        if (this.armorEnchantments != null) this.armorEnchantments.stop();
-
-        if (this.starter.getAllyManager() != null) this.starter.getAllyManager().forceRemoveAllies();
-
-        getServer().getOnlinePlayers().forEach(this.starter.getCrazyManager()::unloadCEPlayer);
     }
 
     public Starter getStarter() {
         return this.starter;
     }
 
-    public PluginManager getPluginManager() {
-        return this.pluginManager;
-    }
-
     public BossBarController getBossBarController() {
-        return bossBarController;
+        return null;
     }
 
     public boolean isLogging() {
