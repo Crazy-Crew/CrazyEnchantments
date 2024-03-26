23a05b24b130e1092d30407e6c1e6fdf3ba52e09
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-26 13:33:36 -0400

    Re-enable base gui

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/commands/types/CommandHelp.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/commands/types/CommandHelp.java
@@ -1,6 +1,8 @@
 package com.badbones69.crazyenchantments.paper.commands.types;
 
+import com.badbones69.crazyenchantments.paper.api.builders.types.ShopMenu;
 import com.badbones69.crazyenchantments.paper.api.enums.Messages;
+import com.badbones69.crazyenchantments.paper.api.managers.ShopManager;
 import com.badbones69.crazyenchantments.paper.commands.BaseCommand;
 import dev.triumphteam.cmd.bukkit.annotation.Permission;
 import dev.triumphteam.cmd.core.annotations.Command;
@@ -13,7 +15,9 @@ public class CommandHelp extends BaseCommand {
     @Command
     @Permission("crazyenchantments.gui")
     public void gui(Player player) {
-        //this.listener.openGUI(player);
+        ShopManager shopManager = this.plugin.getStarter().getShopManager();
+
+        player.openInventory(new ShopMenu(player, shopManager.getInventorySize(), shopManager.getInventoryName()).build().getInventory());
     }
 
     @Command("help")
