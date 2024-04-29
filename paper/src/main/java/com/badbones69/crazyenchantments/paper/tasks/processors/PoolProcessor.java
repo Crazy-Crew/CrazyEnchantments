a3ff95df202d86345d266ea5ff8b4c47f2648673
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-29 11:01:23 -0400

    Update pool processor

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/tasks/processors/PoolProcessor.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/tasks/processors/PoolProcessor.java
@@ -1,6 +1,8 @@
 package com.badbones69.crazyenchantments.paper.tasks.processors;
 
 import com.badbones69.crazyenchantments.paper.CrazyEnchantments;
+import com.badbones69.crazyenchantments.paper.scheduler.FoliaRunnable;
+import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -14,7 +16,7 @@ public abstract class PoolProcessor {
 
     private final int maxQueueSize = 10000;
 
-    private int taskId;
+    private ScheduledTask taskId;
 
     public PoolProcessor() {
         start();
@@ -41,7 +43,7 @@ public abstract class PoolProcessor {
      * Terminates the thread pool.
      */
     public void stop() {
-        plugin.getServer().getScheduler().cancelTask(taskId);
+        taskId.cancel();
         executor.shutdown();
         executor = null;
     }
@@ -51,12 +53,13 @@ public abstract class PoolProcessor {
      * This should ensure that with a higher player count, that all tasks are processed.
      */
     private void resizeChecker() {
-        taskId = plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, () -> {
-
-            if ((executor.getQueue().size() / executor.getCorePoolSize() > maxQueueSize / 5) && !(executor.getMaximumPoolSize() <= executor.getCorePoolSize() + 1)) {
-                executor.setCorePoolSize(executor.getCorePoolSize() + 1);
+        taskId = new FoliaRunnable(this.plugin.getServer().getAsyncScheduler(), TimeUnit.SECONDS) {
+            @Override
+            public void run() {
+                if ((executor.getQueue().size() / executor.getCorePoolSize() > maxQueueSize / 5) && !(executor.getMaximumPoolSize() <= executor.getCorePoolSize() + 1)) {
+                    executor.setCorePoolSize(executor.getCorePoolSize() + 1);
+                }
             }
-
-        }, 20, 100);
+        }.runAtFixedRate(plugin, 20, 100);
     }
 }
