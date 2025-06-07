4b75097dca4eab79a99e8cca79421fb77a40e53a
Ryder Belserion <contact@ryderbelserion.com>
2025-06-07 18:51:29 -0400

    Add block vein record

---
+++ paper/src/main/java/com/ryderbelserion/crazyenchantments/paper/enchants/types/pickaxes/veinminer/objects/BlockVein.java
@@ -0,0 +1,5 @@
+package com.ryderbelserion.crazyenchantments.paper.enchants.types.pickaxes.veinminer.objects;
+
+import org.bukkit.block.Block;
+
+public record BlockVein(Block block, int distance, int experience) {}
