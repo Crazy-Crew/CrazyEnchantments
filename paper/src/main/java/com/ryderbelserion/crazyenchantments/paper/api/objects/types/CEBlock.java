0d3b3449370fe87dc1954a50eee415cb63116bca
Ryder Belserion <contact@ryderbelserion.com>
2025-06-15 19:46:43 -0400

    Add custom block object

---
+++ paper/src/main/java/com/ryderbelserion/crazyenchantments/paper/api/objects/types/CEBlock.java
@@ -0,0 +1,39 @@
+package com.ryderbelserion.crazyenchantments.paper.api.objects.types;
+
+import org.bukkit.block.Block;
+import org.jetbrains.annotations.NotNull;
+
+public class CEBlock {
+
+    private final int distance;
+    private final Block block;
+    private final String type;
+
+    public CEBlock(@NotNull final Block block, final int distance) {
+        this.block = block;
+        this.type = block.getType().getKey().asString();
+        this.distance = distance;
+    }
+
+    private int experience = 0;
+
+    public void setExperience(final int experience) {
+        this.experience = experience;
+    }
+
+    public final Block getBlock() {
+        return this.block;
+    }
+
+    public final int getExperience() {
+        return this.experience;
+    }
+
+    public final int getDistance() {
+        return this.distance;
+    }
+
+    public final String getType() {
+        return this.type;
+    }
+}
