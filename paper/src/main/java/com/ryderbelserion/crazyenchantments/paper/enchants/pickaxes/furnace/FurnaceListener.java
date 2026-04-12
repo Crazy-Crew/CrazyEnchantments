5dfcbeb291644a5b93fc002e693755efda2022f2
Ryder Belserion <contact@ryderbelserion.com>
2026-04-12 02:26:30 -0400

    Add a check to see if an item can be smelted.
    This is done getting the server's recipes, iterating over them... checking if it's a furnace recipe, fetching input choice, and then running choice#test
    
    Untested, I can't find which override method internally it runs yet... but once I test it, and benchmark/breakpoint it to make sure I am happy with the result. we good

--- paper/src/main/java/com/ryderbelserion/crazyenchantments/paper/enchants/pickaxes/furnace/FurnaceListener.java
+++ paper/src/main/java/com/ryderbelserion/crazyenchantments/paper/enchants/pickaxes/furnace/FurnaceListener.java
@@ -6,14 +6,21 @@ import com.ryderbelserion.crazyenchantments.paper.api.registry.enchants.Enchantm
 import io.papermc.paper.registry.RegistryAccess;
 import io.papermc.paper.registry.RegistryKey;
 import net.kyori.adventure.key.Key;
+import net.minecraft.world.item.crafting.RecipePropertySet;
 import org.bukkit.Registry;
+import org.bukkit.Server;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Item;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDropItemEvent;
+import org.bukkit.inventory.FurnaceRecipe;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.Recipe;
+import org.bukkit.inventory.RecipeChoice;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Iterator;
 import java.util.List;
 
 public class FurnaceListener implements Listener {
@@ -25,6 +32,7 @@ public class FurnaceListener implements Listener {
     private final EnchantmentRegistry enchantmentRegistry;
     private final boolean isYardWatchEnabled;
     private final CrazyPlugin plugin;
+    private final Server server;
 
     public FurnaceListener(@NotNull final CrazyPlugin plugin, @NotNull final EnchantmentRegistry enchantmentRegistry) {
         this.enchantmentRegistry = enchantmentRegistry;
@@ -32,6 +40,7 @@ public class FurnaceListener implements Listener {
         final CrazyEnchantmentsPaper platform = plugin.getPlatform();
 
         this.isYardWatchEnabled = platform.isYardWatchEnabled();
+        this.server = plugin.getServer();
         this.plugin = plugin;
     }
 
@@ -49,10 +58,37 @@ public class FurnaceListener implements Listener {
             final ItemStack itemStack = item.getItemStack();
 
             // check if it can be smelted, if not continue.
+            if (!canSmelt(itemStack)) {
+                continue;
+            }
 
             item.setItemStack(itemStack);
 
             event.getItems().set(position, item);
         }
+    }
+
+    public boolean canSmelt(@NotNull final ItemStack itemStack) {
+        boolean canSmelt = false;
+
+        final Iterator<Recipe> recipes = this.server.recipeIterator();
+
+        while (recipes.hasNext()) {
+            final Recipe recipe = recipes.next();
+
+            if (!(recipe instanceof FurnaceRecipe furnace)) {
+                continue;
+            }
+
+            final RecipeChoice choice = furnace.getInputChoice();
+
+            canSmelt = choice.test(itemStack);
+
+            if (canSmelt) {
+                break;
+            }
+        }
+
+        return canSmelt;
     }
 }
