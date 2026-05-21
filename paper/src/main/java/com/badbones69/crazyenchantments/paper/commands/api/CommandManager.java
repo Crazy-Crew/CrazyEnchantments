a3f1d72dd47e7ca90f8ad454b9671b7d43ab46a4
Ryder Belserion <contact@ryderbelserion.com>
2026-05-21 17:56:45 -0400

    Add missing imports

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/commands/api/CommandManager.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/commands/api/CommandManager.java
@@ -1,22 +1,39 @@
 package com.badbones69.crazyenchantments.paper.commands.api;
 
 import com.badbones69.crazyenchantments.paper.CrazyEnchantments;
+import com.badbones69.crazyenchantments.paper.Methods;
+import com.badbones69.crazyenchantments.paper.Starter;
+import com.badbones69.crazyenchantments.paper.api.CrazyManager;
+import com.badbones69.crazyenchantments.paper.api.objects.CEnchantment;
+import com.badbones69.crazyenchantments.paper.api.objects.Category;
+import com.badbones69.crazyenchantments.paper.api.utils.ColorUtils;
 import com.badbones69.crazyenchantments.paper.commands.api.relations.ArgumentRelations;
-import com.badbones69.crazyenchantments.paper.commands.types.BlackSmithCommand;
+import com.badbones69.crazyenchantments.paper.commands.types.player.single.BlackSmithCommand;
+import com.badbones69.crazyenchantments.paper.commands.types.player.single.TinkerCommand;
+import com.badbones69.crazyenchantments.paper.controllers.settings.EnchantmentBookSettings;
 import com.ryderbelserion.fusion.paper.builders.items.PlayerBuilder;
 import dev.triumphteam.cmd.bukkit.BukkitCommandManager;
 import dev.triumphteam.cmd.core.suggestion.SuggestionKey;
+import io.papermc.paper.registry.RegistryAccess;
+import io.papermc.paper.registry.RegistryKey;
 import org.bukkit.Server;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.jetbrains.annotations.NotNull;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class CommandManager {
 
     private static final CrazyEnchantments plugin = CrazyEnchantments.getPlugin(CrazyEnchantments.class);
+    private static final Starter starter = plugin.getStarter();
+    private static final Methods methods = starter.getMethods();
+    private static final CrazyManager crazyManager = starter.getCrazyManager();
     private static final Server server = plugin.getServer();
+    private static final EnchantmentBookSettings book = starter.getEnchantmentBookSettings();
 
     private static final BukkitCommandManager<CommandSender> commandManager = BukkitCommandManager.create(plugin);
 
@@ -96,7 +113,17 @@ public class CommandManager {
         commandManager.registerArgument(PlayerBuilder.class, (_, context) -> new PlayerBuilder(context));
 
         List.of(
-                new BlackSmithCommand()
+                new BlackSmithCommand(),
+                new TinkerCommand()
+
+                //new ConvertCommand(),
+                //new ReloadCommand(),
+                //new CheckCommand(),
+                //new DebugCommand(),
+                //new FixCommand(),
+
+                //new LimitCommand(),
+                //new HelpCommand()
         ).forEach(commandManager::registerCommand);
     }
 
