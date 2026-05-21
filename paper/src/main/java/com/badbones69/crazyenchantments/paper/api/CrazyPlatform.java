f8dd56b6dedbc8e0a04cd62e73969a94d128e376
Ryder Belserion <contact@ryderbelserion.com>
2026-05-21 17:57:53 -0400

    Add getPlayer method

--- paper/src/main/java/com/badbones69/crazyenchantments/paper/api/CrazyPlatform.java
+++ paper/src/main/java/com/badbones69/crazyenchantments/paper/api/CrazyPlatform.java
@@ -4,18 +4,23 @@ import com.badbones69.crazyenchantments.paper.CrazyEnchantments;
 import com.ryderbelserion.fusion.core.api.enums.Level;
 import com.ryderbelserion.fusion.paper.FusionPaper;
 import com.ryderbelserion.fusion.paper.files.PaperFileManager;
+import org.bukkit.Server;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-
+import org.jspecify.annotations.NonNull;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Stream;
 
 public class CrazyPlatform {
 
     private final CrazyEnchantments plugin = JavaPlugin.getPlugin(CrazyEnchantments.class);
+
+    private final Server server = this.plugin.getServer();
 
     private final FusionPaper fusion = this.plugin.getFusion();
 
@@ -57,5 +62,9 @@ public class CrazyPlatform {
                 "Messages.yml",
                 "Tinker.yml"
         ).forEach(file -> this.fileManager.extractFile(file, examples.resolve(file)));
+    }
+
+    public Optional<Player> getPlayer(@NonNull final String name) {
+        return Optional.ofNullable(this.server.getPlayer(name));
     }
 }
